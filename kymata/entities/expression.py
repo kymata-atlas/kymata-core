8fc1587d71383171d1b17206bed2039f8b1d41af
Cai Wingfield <cai@caiwingfield.net>
2023-09-29 20:46:11 +0100

    Successful extraction of data from matlab files

--- kymata/entities/expression.py
+++ kymata/entities/expression.py
@@ -15,24 +15,25 @@ from kymata.io.matlab import load_mat
 _data_matrix_arg = sparray | ndarray
 
 
-class Expression:
+#  TODO: this should be able to contain arbitrary quantities of functions
+#  TODO: and have merge/split/slice functionality
+class ExpressionSet:
     """
     Brain data associated with expression of a single function.
     Includes lh, rh, flipped, non-flipped.
     """
     def __init__(self,
-                 name: str,
+                 function_name: str,
                  # Metadata
-                 tmin: float, tstep: float,
-                 vertices: Sequence[int],
-                 latencies: Sequence[float],
+                 hexels: Sequence[int],
+                 latencies: Sequence[float],  # Seconds
                  # Underlying data
                  lh_data: _data_matrix_arg,
                  rh_data: _data_matrix_arg,
                  lh_flipped_data: _data_matrix_arg,
                  rh_flipped_data: _data_matrix_arg):
 
-        self.name: str = name
+        self.function_name: str = function_name
 
         self._data_lh_sparse: dok_array = dok_array(lh_data)
         self._data_rh_sparse: dok_array = dok_array(rh_data)
@@ -40,21 +41,14 @@ class Expression:
         self._data_flipped_rh_sparse: dok_array = dok_array(rh_flipped_data)
         assert all_equal(data.shape for data in (rh_data, lh_data, lh_flipped_data, rh_flipped_data))
 
-        self.vertices: Sequence[int] = vertices
+        self.hexels: Sequence[int] = hexels
         self.latencies: Sequence[float] = latencies
 
         self.n_vertices: int
         self.n_timepoints: int
         self.n_timepoints, self.n_vertices = lh_data.shape
         assert self.n_timepoints == len(self.latencies) == lh_data.shape[0]
-        assert self.n_vertices == len(self.vertices) == lh_data.shape[1]
-
-        self.t_min: float = tmin
-        self.t_step: float = tstep
-        self.t_step: float = tmin + (self.n_timepoints - 1) * tstep  # TODO: Verify I didn't make an out-by-one
-        assert self.latencies[0] == self.t_min
-        if len(latencies) >= 2:
-            assert latencies[0] + self.t_step == latencies[1]
+        assert self.n_vertices == len(self.hexels) == lh_data.shape[1]
 
     # Public data access separated from data storage
 
@@ -84,9 +78,10 @@ def minimise_pmatrix(pmatrix: _data_matrix_arg) -> dok_array:
     raise NotImplementedError()
 
 
-def load_from_matab_expression_files(name: str,
+# TODO: don't in general need to store flipped, so this should be a specialised load_flipped function
+def load_from_matab_expression_files(function_name: str,
                                      lh_file: PathLike, rh_file: PathLike,
-                                     flipped_lh_file: PathLike, flipped_rh_file: PathLike) -> Expression:
+                                     flipped_lh_file: PathLike, flipped_rh_file: PathLike) -> ExpressionSet:
     """Load from a set of MATLAB files."""
     lh_mat = load_mat(Path(lh_file))
     rh_mat = load_mat(Path(rh_file))
@@ -114,15 +109,29 @@ def load_from_matab_expression_files(name: str,
     assert all_equal(mat["outputSTC"]["data"].shape[1]      for mat in all_mats)
     assert lh_mat["outputSTC"]["data"].shape[1] == lh_mat["nVertices"]
 
-    return Expression(
-        name=name,
-        vertices=lh_mat["outputSTC"]["vertices"],
-        latencies=lh_mat["latencies"],
-        tmin=lh_mat["outputSTC"]["tmin"], tstep=lh_mat["outputSTC"]["tstep"],
-        lh_data=        minimise_pmatrix(lh_mat["outputSTC"]["data"]),
-        rh_data=        minimise_pmatrix(lh_mat["outputSTC"]["data"]),
-        lh_flipped_data=minimise_pmatrix(lh_mat["outputSTC"]["data"]),
-        rh_flipped_data=minimise_pmatrix(lh_mat["outputSTC"]["data"]),
+    # If the data has been downsampled
+    if lh_mat["outputSTC"]["tstep"] != lh_mat["latency_step"]/1000:
+        downsample_ratio = (lh_mat["latency_step"] / 1000) / lh_mat["outputSTC"]["tstep"]
+        assert downsample_ratio == int(downsample_ratio)
+        downsample_ratio = int(downsample_ratio)
+    else:
+        downsample_ratio = 1
+
+    def _prep_data(data):
+        return (
+            minimise_pmatrix(
+                _downsample_data(data, downsample_ratio)
+                [:len(lh_mat["latencies"]), :]
+        ))
+
+    return ExpressionSet(
+        function_name=function_name,
+        hexels=lh_mat["outputSTC"]["vertices"],
+        latencies=lh_mat["latencies"] / 1000,
+        lh_data=        _prep_data(lh_mat["outputSTC"]["data"]),
+        rh_data=        _prep_data(lh_mat["outputSTC"]["data"]),
+        lh_flipped_data=_prep_data(lh_mat["outputSTC"]["data"]),
+        rh_flipped_data=_prep_data(lh_mat["outputSTC"]["data"]),
     )
 
 
@@ -130,3 +139,21 @@ def _base_function_name(function_name: str) -> str:
     """Removes extraneous metadata from function names."""
     function_name = function_name.removesuffix("-flipped")
     return function_name
+
+
+def _downsample_data(data, ratio):
+    if ratio == 1:
+        return data
+    else:
+        return data[::ratio, :]
+
+
+if __name__ == '__main__':
+    sample_data_dir = Path(Path(__file__).parent.parent.parent, "data", "sample-data")
+    expression_data = load_from_matab_expression_files(
+        function_name="hornschunck_horizontalPosition",
+        lh_file=Path(sample_data_dir, "hornschunck_horizontalPosition_lh_10242verts_-200-800ms_cuttoff1000_5perms_ttestpval.mat"),
+        rh_file=Path(sample_data_dir, "hornschunck_horizontalPosition_rh_10242verts_-200-800ms_cuttoff1000_5perms_ttestpval.mat"),
+        flipped_lh_file=Path(sample_data_dir, "hornschunck_horizontalPosition-flipped_lh_10242verts_-200-800ms_cuttoff1000_5perms_ttestpval.mat"),
+        flipped_rh_file=Path(sample_data_dir, "hornschunck_horizontalPosition-flipped_rh_10242verts_-200-800ms_cuttoff1000_5perms_ttestpval.mat"),
+    )
