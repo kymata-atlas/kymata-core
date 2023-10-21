28d41e4e3df08c0725ea82a9e96e492ff545e517
neukym <acgt2@cam.ac.uk>
2023-10-21 09:46:17 +0100

    Renames github action script

--- tests/test_kymata-toolbox.py
+++ tests/test_kymata-toolbox.py
@@ -1,3 +1,11 @@
 
-def test_example():
+# Testing functionality in expression class
+
+def test_nkg_load_save_functionality():
+    assert 1 == 1
+
+
+# Test functionality in IPPM generation
+
+def test_ippm_generation():
     assert 1 == 1
