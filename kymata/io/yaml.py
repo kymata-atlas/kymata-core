a96830275527a699da9573b69faaa41a7beb7c5e
neukym <acgt2@cam.ac.uk>
2023-10-29 21:37:18 +0000

    Update kymata/io/yaml.py
    Co-authored-by: Cai <caiw@users.noreply.github.com>

--- kymata/io/yaml.py
+++ kymata/io/yaml.py
@@ -3,7 +3,4 @@ import yaml
 def load_config_parameters(file_location: String):
     '''Load config parameters'''
     with open(file_location, "r") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+        return yaml.safe_load(stream)
