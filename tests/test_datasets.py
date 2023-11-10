702786327630f09d1c80008d8551405c5ef796ea
Cai Wingfield <cai@caiwingfield.net>
2023-11-10 17:29:32 +0000

    Add a second datasets test with matlab files

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 
-from kymata.datasets.sample import get_dataset_kymata_mirror_q3_2023, delete_dataset
+from kymata.datasets.sample import get_dataset_kymata_mirror_q3_2023, delete_dataset, get_dataset_gm_loudness
 
 
 def test_download_and_delete_q3_2023_data_files():
     dataset = get_dataset_kymata_mirror_q3_2023(download=False)
+    try:
+        dataset.download()
+        for filename in dataset.filenames:
+            assert Path(dataset.path, filename).exists()
+    finally:
+        delete_dataset(dataset)
+        for filename in dataset.filenames:
+            assert not Path(dataset.path, filename).exists()
+
+
+def test_download_and_delete_gm_loudness3_data_files():
+    dataset = get_dataset_gm_loudness(download=False)
     try:
         dataset.download()
         for filename in dataset.filenames:
