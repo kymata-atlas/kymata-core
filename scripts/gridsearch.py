98659de7d14f9de71ec7945515dfac5c0c734b3c
Oliver Parish <op02@login-h01.mrc-cbu.cam.ac.uk>
2023-12-07 12:56:37 +0000

    gridsearch working

--- scripts/gridsearch.py
+++ scripts/gridsearch.py
@@ -1,209 +1,165 @@
-from colorama import Fore
-from colorama import Style
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+import time
+import os.path
+import h5py
+from scipy.stats import ttest_ind
+import scipy.stats as stats
 
+def norm(x):
+    x -= np.mean(x, axis=-1, keepdims=True)
+    x /= np.sqrt(np.sum(x**2, axis=-1, keepdims=True))
+    return x
 
-def do_gridsearch(neurophysiology_data_file_directory,
-                    predicted_function_outputs_data,
-                    hexel_expression_master,
-                    functions_to_apply_gridsearch):
+def get_EMEG_data(EMEG_path, need_names=False):
+    if not os.path.isfile(f'{EMEG_path}.npy') or need_names:
+        evoked = mne.read_evokeds(f'{EMEG_path}.fif', verbose=False)  # should be len 1 list
+        EMEG = evoked[0].get_data()  # numpy array shape (sensor_num, N) = (370, 406_001)
+        EMEG /= np.max(EMEG, axis=1, keepdims=True)
+        np.save(f'{EMEG_path}.npy', np.array(EMEG, dtype=np.float16))  # nb fif is float32
+        return EMEG, evoked[0].ch_names
+    else:
+        return np.load(f'{EMEG_path}.npy'), None
+
+def load_function(function_path, func_name, downsample_rate):
+    if not os.path.isfile(function_path + '.h5'):
+        import scipy.io
+        mat = scipy.io.loadmat(function_path + '.mat')['stimulisig']
+        with h5py.File(function_path + '.h5', 'w') as f:
+            for key in mat.dtype.names:
+                if key != 'name':
+                    f.create_dataset(key, data=np.array(mat[key][0,0], dtype=np.float16))
+        func = np.array(mat[func_name][0][0])[:,::downsample_rate]  # shape = (400, 1000)
+    else:
+        with h5py.File(function_path + '.h5', 'r') as f:
+            func = f[func_name][:,::downsample_rate]
+    return func.flatten()
+
+def generate_derangement(n):  # approx 3ms runtime for n=400
+    while True:
+        v = np.arange(n)
+        for j in range(n - 1, -1, -1):
+            p = np.random.randint(0, j + 1)
+            if v[p] == j:
+                break
+            else:
+                v[j], v[p] = v[p], v[j]
+        else:
+            return v
+
+def ttest(corrs, f_alpha=0.001, use_all_lats=True):
+    # Vectorised Welch's t-test
+    # Fisher Z-Transformation
+    corrs = 0.5 * np.log((1 + corrs) / (1 - corrs))
+    n_channels, nDerangements, n_splits, T_steps = corrs.shape
+
+    true_mean = np.mean(corrs[:, 0, :, :], axis=1)
+    true_var = np.var(corrs[:, 0, :, :], axis=1, ddof=1)
+    true_n = n_splits
+    if use_all_lats:
+        rand_mean = np.mean(corrs[:, 1:, :, :].reshape(n_channels, -1), axis=1).reshape(n_channels, 1)
+        rand_var = np.var(corrs[:, 1:, :, :].reshape(n_channels, -1), axis=1, ddof=1).reshape(n_channels, 1)
+        rand_n = n_splits * nDerangements * T_steps
+    else:
+        rand_mean = np.mean(corrs[:, 1:, :, :].reshape(n_channels, -1, T_steps), axis=1)
+        rand_var = np.var(corrs[:, 1:, :, :].reshape(n_channels, -1, T_steps), axis=1, ddof=1)
+        rand_n = n_splits * nDerangements
+
+    # Vectorized two-sample t-tests for all channels and time steps
+    numerator = true_mean - rand_mean
+    denominator = np.sqrt(true_var / true_n + rand_var / rand_n)
+    df = ((true_var / true_n + rand_var / rand_n) ** 2 /
+        ((true_var / true_n) ** 2 / (true_n - 1) +
+        (rand_var / rand_n) ** 2 / (rand_n - 1)))
+
+    t_stat = numerator / denominator
+    p = stats.t.sf(np.abs(t_stat), df) * 2  # two-tailed p-value
+
+    # Adjust p-values for multiple comparisons (Bonferroni correction) [NOT SURE ABOUT THIS]
+    pvalues_adj = p #np.minimum(1, p * T_steps * n_channels / (1 - f_alpha))
+
+    return pvalues_adj
+
+def do_gridsearch(data_root='/imaging/projects/cbu/kymata/data',
+                  data_path='/dataset_4-english-narratives/intrim_preprocessing_files/3_trialwise_sensorspace/evoked_data',
+                  function_root='/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/predicted_function_contours',
+                  function_path='/GMSloudness/stimulisig',
+                  func_name='d_IL2',
+                  audio_shift_correction=0.5375,
+                  seconds_per_split=0.5,
+                  n_splits=800,
+                  downsample_rate=5,
+                  nDerangements=1,
+                  rep='-ave',
+                  save_pvalues_path=None,
+                  p='participant_01',
+                  start_lat=-100):
     '''Do the Kymata gridsearch over all hexels for all latencies'''
 
-    print(f"{Fore.GREEN}{Style.BRIGHT}Starting gridsearch{Style.RESET_ALL}")
-
-    # Load variables
-    functionname = 'TVL2002 Overall instantaneous loudness'
-    functionname = 'xxxx'
-    inputstream = 'audio';
-
-    experimentName = ['DATASET_3-01_visual-and-auditory'];
-    itemlistFilename = [rootDataSetPath, experimentName, '/items.txt'];
-
-    pre_stimulus_window = 200 # in milliseconds
-    post_stimulus_window = 800 # in milliseconds
-
-    latency_step = 5 # in milliseconds
-    latencies = [-200:latency_step:800]
-    number_of_iterations = 5
-    downsample_rate = 4
-    cuttoff = 1000 # in milliseconds
-
-    print(f"...for Function: {xxxxx}")
-
-        Check if the master expression already contains it
-
-        Print out similar, and check.
-
-        for hemi in ['left', 'right']:
-
-            print(f"...for {hemi}")
-
-            # Polarity represents a special case of the function search - that
-            # where the waveform is flipped in it's polarity. The default action
-            # in Kymata is to check both, and then select the best, as we don't
-            # care about the polarity.
-            for polaritiy in ['positive', 'negitive']
-
-                print(f"   ...for {polaritiy}")
-
-                do gridsearch_for_polarity() #??? do we need this at all and just use the two tailness?
-
-            print(f"   ...merging polarities")
-
-            merge polarities
-
-
-
-    print(f"{Fore.GREEN}{Style.BRIGHT}Ending Gridsearch. Saving in master expression file. {Style.RESET_ALL}")
-
-    return xx
-
-
-def do_gridsearch_for_polarity(polarity:String) -> STC file:
-    '''Do the gridsearch for a given polarity'''
-
-    print(f"   ...XYZ")
-
-    dentritic current_data_array = new array()
-    prediction_waveform = load_data.load_prediction_waveform()
-
-    if polarity = 'negative'
-        prediction_waveform = prediction_waveform * -1
-
-    vecotrise and use Numba/CyPY/CUDA or NUMBa and ...
-    # cut to trial length
-    wordsignalstrue = wordsignalstrue(:, 1: cutoff)';
-    wordsignalstrue = repmat(wordsignalstrue, [1 1 nVertices]);
-    wordsignalstrue = permute(wordsignalstrue, [3 1 2]);
-    wordsignalstrue = single(wordsignalstrue);
-    % wordsignalstrue(wordsignalstrue == 0) = nan;
-
-    # create permorder
-    [junk permorder] = sort(rand(400, nWords), 2);
-    permorder = unique([1:nWords; permorder], 'rows');
-    permorder(any(bsxfun( @ minus, permorder, 1: nWords) == 0, 2),:) = []; permorder = [1:nWords; permorder];
-    permorder = permorder(1:numberOfIterations + 1,:);
-
-    # Convert MEG - signals true, in same order as wordlist
-
-    allMEGdata = single(zeros(nVertices, nTimePoints, nWords));
-
-    for w in 1:numel(wordlist):
-        thisword = char(wordlist(w));
-
-        inputfilename = [rootCodeOutputPath version '/' experimentName, '/5-averaged-by-trial-data/', inputfolder, '/', thisword
-                     '-' leftright '.stc'];
-
-        disp(num2str(w));
-
-        sensordata = mne_read_stc_file(inputfilename);
-        allMEGdata(:,:, w) = sensordata.data(:,:)
-
-    clear('-regexp', '^grand');
-    clear other as well
-
-    # Start Matching / Mismatching proceedure for each time point
-    for latency in latencies:
-
-        print(f"   ...Latency ', num2str(q) ' out of ', num2str(length(latencies))]")
-
-        MEGdata = allMEGdata(:, (pre_stimulus_window + latency + 1): (pre_stimulus_window + cutoff + latency),:);
-
-        # downsample
-        MEGdataDownsampled = MEGdata(:, 1: downsample_rate:end,:);
-        clear MEGdata;
-        wordsignalstrueDownsampled = wordsignalstrue(:, 1: downsample_rate:end,:);
-
-        if debug:
-            scatter(MEGdata(1,:, 1), wordsignalstrue(1,:, 1));
-            scatter(MEGdataDownsampled(1,:, 1), wordsignalstrueDownsampled(1,:, 1));
-
-        # -------------------------------
-        # Matched / Mismatched
-        # -------------------------------
-
-        allcorrs = zeros(nVertices, numberOfIterations, nWords);
-
-        for i in 1:numberOfIterations:
-
-            disp(['Iteration ', num2str(i) ' out of ', num2str(numberOfIterations)]);
-
-            shuffledMEGdata = permute(MEGdataDownsampled, [3 2 1])
-            downsampledtimespan = size(shuffledMEGdata, 2)
-            shuffledMEGdata = reshape(shuffledMEGdata, nWords, nVertices * downsampledtimespan)
-            shuffledMEGdata = shuffledMEGdata(permorder(i,:),:)
-            shuffledMEGdata = reshape(shuffledMEGdata, nWords, downsampledtimespan, nVertices)
-            shuffledMEGdata = permute(shuffledMEGdata, [3 2 1])
-
-            # Confirm signals are not shorter than cutt-off
-            assert XYZ
-
-            # Do  correlation
-            allcorrs(:, i,:) = nansum(bsxfun( @ minus, shuffledMEGdata,
-                                      nanmean(shuffledMEGdata, 2)). * bsxfun( @ minus, wordsignalstrueDownsampled, nanmean(
-            wordsignalstrueDownsampled, 2)), 2)./ (sqrt(
-            nansum((bsxfun( @ minus, shuffledMEGdata, nanmean(shuffledMEGdata, 2))). ^ 2, 2)). * sqrt(
-            nansum((bsxfun( @ minus, wordsignalstrueDownsampled, nanmean(wordsignalstrueDownsampled, 2))). ^ 2, 2)));
-
-
-        clear shuffledMEGdata;
-
-        # Transform populations with fisher-z
-
-        # First eliviate rounding of - 0.999999 causing problems with log() in fisher-z transform.
-        allcorrs(allcorrs < -0.999999) = -0.999999;
-        allcorrs(allcorrs > 0.999999) = 0.999999;
-
-        # Transform fisher-z
-        allcorrs = 0.5 * log((1 + allcorrs). / (1 - allcorrs));
-
-        truewordsCorr = reshape(allcorrs(:, 1,:), nVertices, nWords, 1);
-        randwordsCorr = reshape(allcorrs(:, 2: end,:), nVertices, (nWords * numberOfIterations) - nWords, 1);
-
-        if debug:
-                # lillefor test for Guassianism
-                vertexnumber = 2444;
-                h = lillietest(randwordsCorr(vertexnumber,:));
-                histfit(randwordsCorr(vertexnumber,:), 40); # plot
-                histfit(truewordsCorr(vertexnumber,:), 40);
-                h = findobj(gca, 'Type', 'patch');
-                display(h);
-                set(h(1), 'FaceColor', [0.982 0.909 0.721], 'EdgeColor', 'k');
-                set(h(2), 'FaceColor', [0.819 0.565 0.438], 'EdgeColor', 'k');
-                And do the little graph disribution box plots below as well.
-
-        # Do population test on signals
-
-        pvalues = zeros(1, nVertices);
-        for vertex = 1:nVertices:
-            truepopulation = truewordsCorr(vertex,:);
-            randpopulation = randwordsCorr(vertex,:);
-
-            # 2-sample t-test
-
-            - Can we do 2 tails to do polarities?
-
-            [h, p, ci, stats] = ttest2(truepopulation, randpopulation,
-                                       1 - ((1 - f_alpha) ^ (1 / (2 * length(latencies) * nVertices))), 'right', 'unequal');
-            pvalues(1, vertex) = p
-
-            # Save at correct latency in STC
-            outputSTC.data((latency) + (pre_stimulus_window + 1),:) = pvalues
-
-            clear MEGdata wordsignalstrueDownsampled MEGdataDownsampled R  randwordsGuassian truewordsGuassian  truewords randwords;
-
-
-
-def do_ad_hoc_power_calculation_on_XYZ(n_samples, alpha, power_val)
-    'This works out the power needed on ZYX'
-
-    n_samples = 15000
-    h number of samples per group
-    alpha = 0.0000005  # significance level
-    power_val = 0.8  # desired power
-    standard_deviation = xyz
-    ratio = n_samples1/n_samples2
-
-    # calculate the minimum detectable effect size
-    meff = power.tt_ind_solve_power(alpha=alpha, power=power_val, nobs1=n_samples, ratio=ratio, alternative='larger', sd = sd)
-
-    print(f"Minimum detectable effect size: {meff:.3f}")
+    EMEG_path = f'{data_root}/{data_path}/{p}{rep}'
+    t0 = time.time()
+
+    # Load data
+    T_steps = int((2000 * seconds_per_split) // downsample_rate)
+    EMEG, EMEG_names = get_EMEG_data(EMEG_path, save_pvalues_path)
+    func = load_function(function_root + function_path, 
+                         func_name=func_name,
+                         downsample_rate=downsample_rate)
+    func = func.reshape(n_splits, T_steps // 2)
+    n_channels = EMEG.shape[0]
+
+    t1 = time.time(); print(f'Load time: {round(t1-t0, 2)}s')
+
+    # Reshape EMEG into splits of 'seconds_per_split' s
+    second_start_points = [start_lat + 200 + round((1000 + audio_shift_correction) * seconds_per_split * i) for i in range(n_splits)]  # correcting for audio shift in delivery
+    R_EMEG = np.zeros((n_channels, n_splits, T_steps))
+    for i in range(n_splits):
+        R_EMEG[:,i,:] = EMEG[:, second_start_points[i]:second_start_points[i] + int(2000 * seconds_per_split):downsample_rate]
+
+    # Get derangement for null dist:
+    permorder = np.zeros((nDerangements, n_splits), dtype=int)
+    for i in range(nDerangements):
+        permorder[i, :] = generate_derangement(n_splits)
+    permorder = np.vstack((np.arange(n_splits), permorder))
+
+    # FFT cross-corr
+    R_EMEG = np.fft.rfft(norm(R_EMEG), n=T_steps, axis=-1)
+    F_func = np.conj(np.fft.rfft(norm(func), n=T_steps, axis=-1))
+    corrs = np.zeros((n_channels, nDerangements+1, n_splits, T_steps//2))
+    for i, order in enumerate(permorder):
+        deranged_EMEG = R_EMEG[:, order, :]
+        corrs[:, i] = np.fft.irfft(deranged_EMEG * F_func)[:, :, :T_steps//2]
+
+    t2 = time.time(); print(f'Corr time: {round(t2-t1, 2)}s')
+
+    plt.figure(1)
+    plt.plot(np.linspace(start_lat, start_lat + 1000 * seconds_per_split, T_steps//2), np.mean(corrs[[207, 210, 5, 10], 0], axis=-2).T)
+    plt.plot(np.linspace(start_lat, start_lat + 1000 * seconds_per_split, T_steps//2), np.mean(corrs[0:300:15, 1], axis=-2).T, 'cyan')
+    plt.axvline(0, color='k')
+    plt.savefig('testing_savefig.png')
+    plt.clf()
+
+    t2 = time.time()
+
+    pvalues = ttest(corrs)
+
+    t3 = time.time(); print(f'T-Test time: {round(t3-t2,2)}s' )
+    
+    plt.figure(2)
+    plt.plot(np.linspace(start_lat, start_lat + 1000 * seconds_per_split, T_steps//2), -np.log(pvalues[[207,210,5,10]].T))
+    plt.axvline(0, color='k')
+    plt.savefig('testing_fig2.png')
+    plt.clf()
+
+    if save_pvalues_path:
+        with h5py.File(save_pvalues_path + '.h5', 'w') as f:
+            f.create_dataset('pvalues', data=pvalues)
+            f.create_dataset('latencies', data=np.linspace(start_lat, int(start_lat + (1000 * seconds_per_split)), 1000//downsample_rate))
+            f.create_dataset('EMEG_names', data=EMEG_names)
+
+    return
+
+
+if __name__ == "__main__":
+    do_gridsearch()
