cc2eb954d6c6ba102b091aadffca7ebaa59ce53a
Cai Wingfield <cai@caiwingfield.net>
2024-01-15 15:43:09 +0000

    Add inverse operator into invoker script

--- invokers/sh/run_gridsearch.sh
+++ invokers/sh/run_gridsearch.sh
@@ -4,4 +4,6 @@ python invokers/run_gridsearch.py \
   --data-path "intrim_preprocessing_files/3_trialwise_sensorspace/evoked_data" \
   --function-path "predicted_function_contours/GMSloudness/stimulisig" \
   --function-name "d_IL2" \
-  --emeg-file "participant_01-ave"
+  --emeg-file "participant_01-ave" \
+  --inverse-operator "intrim_preprocessing_files/4_hexel_current_reconstruction/inverse-operators" \
+  --overwrite
