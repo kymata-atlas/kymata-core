87840993783c6bdfe3f36a1f902532f5768b5e9a
Cai Wingfield <cai@caiwingfield.net>
2024-01-15 15:21:05 +0000

    sample invoker arguments in a script

---
+++ invokers/sh/gridsearch.sh
@@ -0,0 +1,7 @@
+python invokers/run_gridsearch.py \
+  --reality-check-plot-name "example" \
+  --base-dir "/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/" \
+  --data-path "intrim_preprocessing_files/3_trialwise_sensorspace/evoked_data" \
+  --function-path "predicted_function_contours/GMSloudness/stimulisig" \
+  --function-name "d_IL2" \
+  --emeg-file "participant_01-ave"
