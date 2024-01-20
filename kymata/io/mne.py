ea6d6ef6157813ae153eefc878fc4bd2dd52886d
neukym <acgt2@cam.ac.uk>
2024-01-20 20:58:00 +0000

    Update mne.py

--- kymata/io/mne.py
+++ kymata/io/mne.py
@@ -34,7 +34,7 @@ def load_single_emeg(emeg_path, need_names=False, inverse_operator=None, snr=4):
 
 def inverse_operate(evoked, inverse_operator, snr=4):
     lambda2 = 1.0 / snr ** 2
-    inverse_operator = minimum_norm.read_inverse_operator(inverse_operator, verbose=False)
+    inverse_operator = minimum_norm.read_inverse_operator(inverse_operator, verbose=True)
     set_eeg_reference(evoked, projection=True, verbose=False)
     stc = minimum_norm.apply_inverse(evoked, inverse_operator, lambda2, 'MNE', pick_ori='normal', verbose=False)
     print("Inverse operator applied")
