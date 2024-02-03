377d277d14cbb3d04a037fee595c02944586b77f
Cai <caiw@users.noreply.github.com>
2024-02-03 22:19:25 +0000

    Load and apply morph maps when directory is specified (#145)
    * Load an apply morph maps when directory is specified
    
    * Help text: only relevant to source space
    
    * Morph-map filename pattern
    
    * Warn in case averaging but not morphing
    
    * Swithc to --morph bool cli arg
    
    * Rename variable noop
    
    * Bugfix
    
    * Bug fixes
    
    * Update submit_gridsearch.sh
    
    * fixes #148
    
    * bugfix
    
    * Update submit_gridsearch.sh
    
    * rename to single participant override
    
    ---------
    
    Co-authored-by: neukym <acgt2@cam.ac.uk>

--- invokers/run_gridsearch.py
+++ invokers/run_gridsearch.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import argparse
 import time
+from warnings import warn
 
 from kymata.datasets.data_root import data_root_path
 from kymata.gridsearch.plain import do_gridsearch
 from kymata.io.functions import load_function
 from kymata.io.mne import load_emeg_pack
 from kymata.io.nkg import save_expression_set
+from kymata.io.yaml import load_config
 from kymata.plot.plot import expression_plot
 
 _default_output_dir = Path(data_root_path(), "output")
@@ -21,8 +23,10 @@ def main():
                         help='sampling rate of the emeg machine (not implemented yet)')
     parser.add_argument('--snr', type=float, default=3, help='inverse solution snr')
     parser.add_argument('--downsample-rate', type=int, default=5, help='downsample_rate')
-    parser.add_argument('--base-dir', type=str, default='/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/', help='base data directory')
-    parser.add_argument('--data-path', type=str, default='intrim_preprocessing_files/3_trialwise_sensorspace/evoked_data', help='data path after base dir')
+    parser.add_argument('--base-dir', type=str, default='/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/',
+                        help='base data directory')
+    parser.add_argument('--emeg-dir', type=str, default='intrim_preprocessing_files/3_trialwise_sensorspace/evoked_data/',
+                        help='emeg directory, relative to base dir')
     parser.add_argument('--function-path', type=str, default='predicted_function_contours/GMSloudness/stimulisig', help='location of function stimulisig')
     parser.add_argument('--save-expression-set-location', type=Path, default=Path(_default_output_dir),
                         help="Save the results of the gridsearch into an ExpressionSet .nkg file")
@@ -30,12 +34,16 @@ def main():
                         help="Save an expression plots, and other plots, in this location")
     parser.add_argument('--overwrite', action="store_true", help="Silently overwrite existing files.")
     parser.add_argument('--function-name', type=str, default="IL", help='function name in stimulisig')
-    parser.add_argument('--emeg-file', type=str, default="participant_01-ave", help='emeg_file_name')
+    parser.add_argument('--single-participant-override', type=str, default=None, required=False,
+                        help='Supply to run only on one participant')
+    parser.add_argument('--morph', action="store_true",
+                        help="Morph hexel data to fs-average space prior to running gridsearch. "
+                             "Only has an effect if an inverse operator is specified.")
     parser.add_argument('--ave-mode', type=str, default="ave",
                         help='either ave or add, either average over the list of repetitions or treat them as extra data')
     parser.add_argument('--inverse-operator-dir', type=str, default=None, help='inverse solution path')
-    parser.add_argument('--inverse-operator-name', type=str, default="participant_01_ico5-3L-loose02-cps-nodepth-fusion-inv.fif",
-                        help='inverse solution name')
+    parser.add_argument('--inverse-operator-suffix', type=str, default="_ico5-3L-loose02-cps-nodepth-fusion-inv.fif",
+                        help='inverse solution suffix')
     parser.add_argument('--seconds-per-split', type=float, default=0.5,
                         help='seconds in each split of the recording, also maximum range of latencies being checked')
     parser.add_argument('--n-splits', type=int, default=800,
@@ -51,55 +59,54 @@ def main():
     args = parser.parse_args()
     args.base_dir = Path(args.base_dir)
 
-    emeg_dir = Path(args.base_dir, args.data_path)
-    emeg_paths = [Path(emeg_dir, args.emeg_file)]
+    config = load_config(str(Path(Path(__file__).parent.parent, "kymata", "config", "dataset4.yaml")))
 
-    participants = ['pilot_01',
-                    'pilot_02',
-                    'participant_01',
-                    'participant_01b',
-                    'participant_02',
-                    'participant_03',
-                    'participant_04',
-                    'participant_05',
-                    'participant_07',
-                    'participant_08',
-                    'participant_09',
-                    'participant_10',
-                    'participant_11',
-                    'participant_12',
-                    'participant_13',
-                    'participant_14',
-                    'participant_15',
-                    'participant_16',
-                    'participant_17'
-                    ]
+    participants = [
+        'pilot_01',
+        'pilot_02',
+        'participant_01',
+        'participant_01b',
+        'participant_07',
+    ]
 
+    # TODO: move ave-vs-reps choice up to the function interface
     reps = [f'_rep{i}' for i in range(8)] + ['-ave']
-
-    # emeg_paths = [Path(emeg_dir, p + r) for p in participants for r in reps[-1:]]
+    if args.emeg_file is not None:
+        emeg_filenames = [args.single_participant_override + "-ave"]
+    else:
+        emeg_filenames = [
+            p + r
+            for p in participants
+            for r in reps[-1:]
+        ]
 
     start = time.time()
 
-    if args.inverse_operator_dir is None:
-        inverse_operator = None
-    else:
-        inverse_operator = Path(args.base_dir, args.inverse_operator_dir, args.inverse_operator_name)
+    if (len(emeg_filenames) > 1) and (not args.morph) and (args.ave_mode == "ave") and (args.inverse_operator_dir is not None):
+        warn(f"Averaging without morphing to a common space. "
+             f"If you are averaging over multiple participants you should morph to a common space.")
 
     # Load data
-    emeg_values, ch_names = load_emeg_pack(emeg_paths,
+    emeg_path = Path(args.base_dir, args.emeg_dir)
+    morph_dir = Path(args.base_dir, "intrim_preprocessing_files", "4_hexel_current_reconstruction", "morph_maps")
+    emeg_values, ch_names = load_emeg_pack(emeg_filenames,
+                                           emeg_dir=emeg_path,
+                                           morph_dir=morph_dir,
+                                           use_morph=args.morph,
                                            need_names=True,
                                            ave_mode=args.ave_mode,
-                                           inverse_operator=inverse_operator,
+                                           inverse_operator_dir=args.inverse_operator_dir,
+                                           inverse_operator_suffix= args.inverse_operator_suffix,
                                            p_tshift=None,
-                                           snr=args.snr)
+                                           snr=args.snr,
+                                           )
 
     func = load_function(Path(args.base_dir, args.function_path),
                          func_name=args.function_name,
                          bruce_neurons=(5, 10))
     func = func.downsampled(args.downsample_rate)
 
-    channel_space = "source" if inverse_operator is not None else "sensor"
+    channel_space = "source" if args.inverse_operator_dir is not None else "sensor"
 
     es = do_gridsearch(
         emeg_values=emeg_values,
