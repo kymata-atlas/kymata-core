85aee3b9a7b79f4d40a603e7585589db45e08e62
Cai <caiw@users.noreply.github.com>
2024-02-09 17:43:52 +0000

    Expression plot interface to visually group families of functions (#163)
    * Fix a bug which would cause an error in case expression plot function colours were specified in a list
    
    * New function for producing colour gradients of colours
    
    * Add a constant_color_dict function too
    
    * Comments

--- kymata/plot/plot.py
+++ kymata/plot/plot.py
@@ -104,7 +104,7 @@ def expression_plot(
         color = {f: color for f in show_only}
     elif isinstance(color, str):
         # List specified, then pair up in order
-        assert len(color) == len(str)
+        assert len(color) == len(show_only)
         color = {f: c for f, c in zip(show_only, color)}
 
     # Default colours
@@ -316,6 +316,7 @@ def _get_yticks(ylim):
     n_major_ticks = int(ylim / _MAJOR_TICK_SIZE) * -1
     last_major_tick = -1 * n_major_ticks * _MAJOR_TICK_SIZE
     return np.linspace(start=0, stop=last_major_tick, num=n_major_ticks + 1)
+
 
 def plot_top_five_channels_of_gridsearch(
         latencies: NDArray[any],
