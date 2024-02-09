889b0a23f6077153df61b0885eb522034755efa5
Cai Wingfield <cai@caiwingfield.net>
2024-02-09 11:16:52 +0000

    Make spacing consistent, and easier to see if args have defaults set

--- invokers/run_gridsearch.py
+++ invokers/run_gridsearch.py
@@ -20,19 +20,24 @@ def main():
     parser = argparse.ArgumentParser(description='Gridsearch Params')
     parser.add_argument('--emeg-sample-rate', type=int, default=1000,
                         help='sampling rate of the emeg machine (not implemented yet)')
-    parser.add_argument('--snr', type=float, default=3, help='inverse solution snr')
-    parser.add_argument('--downsample-rate', type=int, default=5, help='downsample_rate')
-    parser.add_argument('--base-dir', type=str, default='/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/',
+    parser.add_argument('--snr', type=float, default=3,
+                        help='inverse solution snr')
+    parser.add_argument('--downsample-rate', type=int, default=5,
+                        help='downsample_rate')
+    parser.add_argument('--base-dir', type=Path, default=Path('/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/'),
                         help='base data directory')
     parser.add_argument('--emeg-dir', type=str, default='intrim_preprocessing_files/3_trialwise_sensorspace/evoked_data/',
                         help='emeg directory, relative to base dir')
-    parser.add_argument('--function-path', type=str, default='predicted_function_contours/GMSloudness/stimulisig', help='location of function stimulisig')
+    parser.add_argument('--function-path', type=str, default='predicted_function_contours/GMSloudness/stimulisig',
+                        help='location of function stimulisig')
     parser.add_argument('--save-expression-set-location', type=Path, default=Path(_default_output_dir),
                         help="Save the results of the gridsearch into an ExpressionSet .nkg file")
     parser.add_argument('--save-plot-location', type=Path, default=Path(_default_output_dir),
                         help="Save an expression plots, and other plots, in this location")
-    parser.add_argument('--overwrite', action="store_true", help="Silently overwrite existing files.")
-    parser.add_argument('--function-name', type=str, default="IL", help='function name in stimulisig')
+    parser.add_argument('--overwrite', action="store_true",
+                        help="Silently overwrite existing files.")
+    parser.add_argument('--function-name', type=str, default="IL",
+                        help='function name in stimulisig')
     parser.add_argument('--single-participant-override', type=str, default=None, required=False,
                         help='Supply to run only on one participant')
     parser.add_argument('--morph', action="store_true",
@@ -40,7 +45,8 @@ def main():
                              "Only has an effect if an inverse operator is specified.")
     parser.add_argument('--ave-mode', type=str, default="ave", choices=["ave", "add"],
                         help='`ave`: average over the list of repetitions. `add`: treat them as extra data.')
-    parser.add_argument('--inverse-operator-dir', type=str, default=None, help='inverse solution path')
+    parser.add_argument('--inverse-operator-dir', type=str, default=None,
+                        help='inverse solution path')
     parser.add_argument('--inverse-operator-suffix', type=str, default="_ico5-3L-loose02-cps-nodepth-fusion-inv.fif",
                         help='inverse solution suffix')
     parser.add_argument('--seconds-per-split', type=float, default=0.5,
@@ -56,8 +62,8 @@ def main():
     parser.add_argument('--audio-shift-correction', type=float, default=0.000_537_5,
                         help='audio shift correction, for every second of function, add this number of seconds (to the start of the emeg split) per seconds of emeg seen')
     args = parser.parse_args()
-    args.base_dir = Path(args.base_dir)
 
+    # TODO: use config for ppt lists etc
     config = load_config(str(Path(Path(__file__).parent.parent, "kymata", "config", "dataset4.yaml")))
 
     participants = [
