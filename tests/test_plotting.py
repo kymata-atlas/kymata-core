ef5622e1f584d70cb4b905cc929f29b9950b80e4
Cai <caiw@users.noreply.github.com>
2024-03-01 16:45:40 +0000

    Rename tests so they are discovered by pytest (#210)
    * Rename tests so they are discovered by pytest
    
    * Add basic no-error test for expressionset plotting
    
    * Don't need to do this assert, it's tested elsewhere
    
    * Update test_plotting.py
    
    ---------
    
    Co-authored-by: neukym <acgt2@cam.ac.uk>

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,6 +1,7 @@
 from numpy import array, array_equal
 
-from kymata.plot.plot import _get_best_ylim, _MAJOR_TICK_SIZE, _get_yticks, _get_xticks
+from kymata.datasets.sample import delete_dataset
+from kymata.plot.plot import _get_best_ylim, _MAJOR_TICK_SIZE, _get_yticks, _get_xticks, expression_plot
 
 
 def test_best_best_ylim_returns_supplied_ylim():
@@ -22,16 +23,28 @@ def test_small_data_gets_at_least_one_tick():
     assert len(y_ticks) >= 2
 
 
-def get_x_ticks_standard():
+def test_get_x_ticks_standard():
     x_ticks = _get_xticks((-200, 800))
     assert array_equal(x_ticks, array([-200, -100, 0, 100, 200, 300, 400, 500, 600, 700, 800]))
 
 
-def get_x_ticks_smaller():
+def test_get_x_ticks_smaller():
     x_ticks = _get_xticks((-100, 700))
     assert array_equal(x_ticks, array([-100, 0, 100, 200, 300, 400, 500, 600, 700]))
 
 
-def get_x_ticks_non_multiples():
+def test_get_x_ticks_non_multiples():
     x_ticks = _get_xticks((-150, 750))
     assert array_equal(x_ticks, array([-100, 0, 100, 200, 300, 400, 500, 600, 700]))
+
+
+def test_expression_plot_no_error():
+    from kymata.datasets.sample import TVLInsLoudnessOnlyDataset
+    dataset = TVLInsLoudnessOnlyDataset(download=False)
+    try:
+        dataset.download()
+
+        expression_plot(dataset.to_expressionset())
+
+    finally:
+        delete_dataset(dataset)
