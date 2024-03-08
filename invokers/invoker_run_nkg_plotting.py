74bfef4145c54125ca3c612a82914007bb63c3db
Cai Wingfield <cai@caiwingfield.net>
2024-03-08 17:05:44 +0000

    Nicer visualisation demo

--- invokers/invoker_run_nkg_plotting.py
+++ invokers/invoker_run_nkg_plotting.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from kymata.datasets.sample import KymataMirror2023Q3Dataset
 from kymata.io.config import load_config
-from kymata.plot.color import gradient_color_dict
+from kymata.plot.color import gradient_color_dict, constant_color_dict
 from kymata.plot.plot import expression_plot
 
 
@@ -12,29 +12,8 @@ def main():
 
     expression_data = KymataMirror2023Q3Dataset().to_expressionset()
 
-    expression_plot(expression_data[
-                        'vibration detection (RH-Th/P/M)',
-                        'vibration detection (LH-Th/P/M)',
-                        'vibration detection (LH-Ha/P/M)',
-                        'vibration detection (RH-Ha/P/M)',
-                        'CIECAM02 A',
-                        'CIECAM02 a',
-                        'CIELAB a*',
-                        'CIELAB L',
-                        'TVL loudness chan 1 (instantaneous)',
-                        'TVL loudness chan 2 (instantaneous)',
-                        'TVL loudness chan 3 (instantaneous)',
-                        'TVL loudness chan 4 (instantaneous)',
-                        'TVL loudness chan 5 (instantaneous)',
-                        'TVL loudness chan 6 (instantaneous)',
-                        'TVL loudness chan 7 (instantaneous)',
-                        'TVL loudness chan 8 (instantaneous)',
-                        'TVL loudness chan 9 (instantaneous)',
-                    ],
+    expression_plot(expression_data,
                     show_only=[
-                        'vibration detection (LH-Th/P/M)',
-                        'CIECAM02 A',
-                        'CIECAM02 a',
                         'TVL loudness chan 1 (instantaneous)',
                         'TVL loudness chan 2 (instantaneous)',
                         'TVL loudness chan 3 (instantaneous)',
@@ -44,6 +23,9 @@ def main():
                         'TVL loudness chan 7 (instantaneous)',
                         'TVL loudness chan 8 (instantaneous)',
                         'TVL loudness chan 9 (instantaneous)',
+                        'TVL loudness (instantaneous)',
+                        'TVL loudness (short-term)',
+
                     ],
                     color=gradient_color_dict([
                         'TVL loudness chan 1 (instantaneous)',
@@ -55,7 +37,13 @@ def main():
                         'TVL loudness chan 7 (instantaneous)',
                         'TVL loudness chan 8 (instantaneous)',
                         'TVL loudness chan 9 (instantaneous)',
-                        ], start_color="blue", stop_color="purple"),
+                        ], start_color="blue", stop_color="purple")
+                    | constant_color_dict([
+                        'TVL loudness (instantaneous)',
+                    ], color="red")
+                    | constant_color_dict([
+                        'TVL loudness (short-term)1',
+                    ], color="pink"),
                     minimap_config=config,
                     minimap_view="lateral",
                     minimap_surface="inflated",
