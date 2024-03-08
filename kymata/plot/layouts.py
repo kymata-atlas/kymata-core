4f416a391d7bdb5a1f6b7923018224ceb67af6c6
Cai <caiw@users.noreply.github.com>
2024-03-08 23:54:07 +0000

    Display EEG sensor channels on the correct hemisphere in expression plots (#208)
    * Add MNE layout file
    
    * Create EEG-layout-name-channel.yaml
    
    * Renames the channel mappings
    
    ...as @young-x-skyee pointed out I got them wrong.
    
    * Assign EEG sensors to hemispheres based on x ordinate
    
    * Rerun notebook with new plotting updates
    
    * Linting fixes
    
    * Update Layout to use upper instead of lower
    
    ---------
    
    Co-authored-by: neukym <acgt2@cam.ac.uk>

--- kymata/plot/layouts.py
+++ kymata/plot/layouts.py
@@ -2,6 +2,7 @@ import re
 from pathlib import Path
 from typing import NamedTuple
 
+import yaml
 from mne.io import Raw
 from matplotlib import pyplot as plt
 
@@ -33,9 +34,29 @@ def get_meg_sensor_xy() -> dict[str, Point2d]:
     return d
 
 
-def eeg_sensors() -> list[str]:
-    """The names of all EEG sensors."""
-    return [f"EEG{i:03}" for i in range(1, 65)]
+def get_eeg_sensor_xy() -> dict[str, Point2d]:
+    d = dict()
+    with Path(Path(__file__).parent.parent.parent, "kymata-toolbox-data", "sensor_locations",
+              "EEG1005.lay").open("r") as layout_file:
+        for line in layout_file:
+            parts = line.strip().split("\t")
+            x = float(parts[1])
+            y = float(parts[2])
+            name = parts[-1].upper()
+            d[name] = Point2d(x, y)
+    # Get name mapping
+    with Path(Path(__file__).parent.parent.parent, "kymata-toolbox-data", "sensor_locations",
+              "EEG-layout-channel-mappings.yaml").open("r") as eeg_name_mapping_file:
+        mapping = yaml.safe_load(eeg_name_mapping_file)
+    mapping = {k.upper(): v.upper() for k, v in mapping.items()}
+    inverted_mapping = {v: k for k, v in mapping.items()}
+    # Apply name mapping
+    new_d = {
+        inverted_mapping[name]: point
+        for name, point in d.items()
+        if name in inverted_mapping.keys()
+    }
+    return new_d
 
 
 def plot_eeg_sensor_positions(raw_fif: Raw):
--- kymata/plot/plot.py
+++ kymata/plot/plot.py
@@ -15,7 +15,7 @@ from kymata.entities.expression import HexelExpressionSet, ExpressionSet, Sensor
 from kymata.math.p_values import p_to_logp
 from kymata.entities.functions import Function
 from kymata.math.rounding import round_down, round_up
-from kymata.plot.layouts import get_meg_sensor_xy, eeg_sensors
+from kymata.plot.layouts import get_meg_sensor_xy, get_eeg_sensor_xy
 
 # log scale: 10 ** -this will be the ytick interval and also the resolution to which the ylims will be rounded
 _MAJOR_TICK_SIZE = 50
@@ -56,13 +56,21 @@ _left_right_sensors: tuple[AxisAssignment, AxisAssignment] = (
                        sensor
                        for sensor, (x, y) in get_meg_sensor_xy().items()
                        if x <= 0
-                   ] + eeg_sensors()),
+                   ] + [
+                       sensor
+                       for sensor, (x, y) in get_eeg_sensor_xy().items()
+                       if x <= 0
+                   ]),
     AxisAssignment(axis_name="right",
                    axis_channels=[
                        sensor
                        for sensor, (x, y) in get_meg_sensor_xy().items()
                        if x >= 0
-                   ] + eeg_sensors()),
+                   ] + [
+                       sensor
+                       for sensor, (x, y) in get_eeg_sensor_xy().items()
+                       if x >= 0
+                   ]),
 )
 
 
