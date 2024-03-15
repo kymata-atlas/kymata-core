02815824e2ed0fe8ab5e23246e455a99bd04317c
Cai Wingfield <cai@caiwingfield.net>
2024-03-15 09:45:30 +0000

    Add a failing test to highlight the bug

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,8 +1,8 @@
+import pytest
 from numpy import array, array_equal
 
 from kymata.datasets.sample import delete_dataset
 from kymata.plot.plot import _get_best_ylim, _MAJOR_TICK_SIZE, _get_yticks, _get_xticks, expression_plot
-
 
 
 def test_best_best_ylim_returns_supplied_ylim():
@@ -49,3 +49,89 @@ def test_expression_plot_no_error():
 
     finally:
         delete_dataset(dataset)
+
+
+@pytest.fixture
+def eeg_sensors():
+    from kymata.plot.layouts import get_eeg_sensor_xy
+    return list(get_eeg_sensor_xy().keys())
+
+
+@pytest.fixture
+def meg_sensors():
+    from kymata.plot.layouts import get_meg_sensor_xy
+    return list(get_meg_sensor_xy().keys())
+
+
+@pytest.fixture
+def all_sensors(eeg_sensors, meg_sensors):
+    return eeg_sensors + meg_sensors
+
+
+def test_eeg_correct_sensors(eeg_sensors):
+    assert sorted(eeg_sensors) == sorted([
+        "EEG001",
+        "EEG002",
+        "EEG003",
+        "EEG004",
+        "EEG005",
+        "EEG006",
+        "EEG007",
+        "EEG008",
+        "EEG009",
+        "EEG000",
+        "EEG011",
+        "EEG012",
+        "EEG013",
+        "EEG014",
+        "EEG015",
+        "EEG016",
+        "EEG017",
+        "EEG019",
+        "EEG020",
+        "EEG021",
+        "EEG022",
+        "EEG023",
+        "EEG024",
+        "EEG025",
+        "EEG026",
+        "EEG027",
+        "EEG030",
+        "EEG031",
+        "EEG032",
+        "EEG033",
+        "EEG034",
+        "EEG035",
+        "EEG036",
+        "EEG037",
+        "EEG038",
+        "EEG040",
+        "EEG041",
+        "EEG042",
+        "EEG043",
+        "EEG044",
+        "EEG045",
+        "EEG046",
+        "EEG047",
+        "EEG048",
+        "EEG049",
+        "EEG050",
+        "EEG052",
+        "EEG053",
+        "EEG054",
+        "EEG055",
+        "EEG056",
+        "EEG057",
+        "EEG058",
+        "EEG059",
+        "EEG060",
+        "EEG062",
+        "EEG063",
+        "EEG064",
+        "EEG018",
+        "EEG028",
+        "EEG029",
+        "EEG039",
+        "EEG051",
+        "EEG061",
+    ])
