2bada0a0de3c51910ed2d85d1493c9997d5bade7
Cai Wingfield <cai@caiwingfield.net>
2024-05-03 17:20:27 +0100

    Revert "REVERT change to params"
    This reverts commit d7cfec0940cb52d8e4830240ab0e137b11465cf5.

--- submit_gridsearch.sh
+++ submit_gridsearch.sh
@@ -26,11 +26,9 @@ apptainer exec \
     " cd /imaging/projects/cbu/kymata/analyses/andy/kymata-toolbox/ ; \
       export VENV_PATH=~/poetry/ ; \
       \$VENV_PATH/bin/poetry run python -m invokers.run_gridsearch \
-        --config dataset3.yaml \
-        --function-path 'predicted_function_contours/audio/GMloudness/stimulisig' \
-        --function-name ins_loudness shortterm_loudness \
-        --use-inverse-operator \
-        --morph \
+        --config dataset4.yaml \
+        --function-path 'predicted_function_contours/GMSloudness/stimulisig' \
+        --function-name IL STL \
         --overwrite
   "
   #  --snr $ARG # >> result3.txt
