cfa6221bade52dfa32496def4e5e73b11e56de5a
Oliver Parish <op02@login-h02.mrc-cbu.cam.ac.uk>
2024-05-24 11:00:50 +0100

    Explicitly clain not to have found both the .npz and .mat

--- kymata/io/functions.py
+++ kymata/io/functions.py
@@ -46,7 +46,7 @@ def load_function(function_path_without_suffix: PathType, func_name: str, n_deri
             if function_path_without_suffix.with_suffix(".mat").exists():
                 convert_stimulisig_on_disk_mat_to_npz(function_path_without_suffix)
             else:
-                raise FileNotFoundError(function_path_without_suffix.with_suffix(".npz"))
+                raise FileNotFoundError(f"{function_path_without_suffix}: neither .mat nor .npz found.")
 
         assert function_path_without_suffix.with_suffix(".npz").exists()
 
