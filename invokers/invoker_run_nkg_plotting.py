37f52b05e7b08b49baf159f5686dad9ec090cfe5
Cai <caiw@users.noreply.github.com>
2024-05-24 16:00:00 +0100

    Option to plot locations of significant channels in expression_plots (#209)
    * Switch to using pyplot.axes_mosiac
    
    * Rename tests so they are discovered by pytest
    
    * Add basic no-error test for expressionset plotting
    
    * Don't need to do this assert, it's tested elsewhere
    
    * Add a failing test
    
    * WIP: plot hexel locations
    
    * Feeds in location of brain directory from config
    
    also fixes bug in _hexel_minimap_data
    
    * Plot brain on expression-plot figure
    (view is not correct yet)
    
    * Plot left- and right-hemisphere views in the appropriate places
    
    * Replace y-axis label
    
    * Fix linting
    
    * Argument for view name passthrough
    
    * Bugfix: Plot correct colours
    
    * Plot hexels only for functions which are included in show_only
    
    * Passthrough surface name
    
    * Show example of vibration detection
    
    * Update invoker to match PR description
    
    * Nicer visualisation demo
    
    * Add dataset directory name
    
    * Changes smoothing to 2
    
    * fixes STL bug
    
    * Rename type alias
    
    * Auto-download fsaverage surfaces
    
    * Plotting demo notebook includes minimap example
    
    * Switch notebook example to plot the audio stuff we've been working on, because it's more impressive in the default lateral view.
    
    * Close all figures when done running
    
    * Revert changes to plotting invoker
    
    * Remove dupe config file
    
    * Fix a bug with paires axes on sensor data
    
    * Close individual figs
    
    * Lint
    
    * Save fig before exiting
    
    ---------
    
    Co-authored-by: neukym <acgt2@cam.ac.uk>

--- invokers/invoker_run_nkg_plotting.py
+++ invokers/invoker_run_nkg_plotting.py
@@ -1,34 +1,24 @@
 from pathlib import Path
 from os import path
+
 from kymata.io.nkg import load_expression_set
 from kymata.plot.plot import expression_plot
 
 # template invoker for printing out expression set .nkgs
+def main():
+    path_to_nkg_files = Path(Path(path.abspath("")).parent, "kymata-toolbox-data", "output")
 
-path_to_nkg_files = Path(Path(path.abspath("")).parent, "kymata-toolbox-data", "output")
+    expression_data  = load_expression_set(Path( path_to_nkg_files, "combined_TVL_gridsearch.nkg"))
 
-expression_data = load_expression_set(Path( path_to_nkg_files, "IL_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "STL_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL1_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL2_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL3_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL4_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL5_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL6_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL7_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL8_gridsearch.nkg"))
-expression_data += load_expression_set(Path( path_to_nkg_files, "IL9_gridsearch.nkg"))
+    fig = expression_plot(expression_data,
+                          minimap=True,
+                          minimap_view="lateral",
+                          minimap_surface="inflated",
+                          )
 
-expression_plot(expression_data, color = {
-                    'IL': '#b11e34',
-                    'IL1': '#a201e9',
-                    'IL2': '#a201e9',
-                    'IL3': '#a201e9',
-                    'IL4': '#a201e9',
-                    'IL5': '#a201e9',
-                    'IL6': '#a201e9',
-                    'IL7': '#a201e9',
-                    'IL8': '#a201e9',
-                    'IL9': '#a201e9',
-                    'STL': '#d388b5'
-                  })
+    fig.show()
+    fig.savefig(Path(path_to_nkg_files, "expression_plot.png"))
+
+
+if __name__ == '__main__':
+    main()
