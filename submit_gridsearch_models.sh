f14a4ada37701e86c5b58607a8a091075c8a6dd9
young-x-skyee <young.x.skyee@outlook.com>
2024-05-31 10:22:12 +0100

    update

--- submit_gridsearch_models.sh
+++ submit_gridsearch_models.sh
@@ -7,8 +7,8 @@
 
 
 #SBATCH --job-name=gridsearch
-#SBATCH --output=kymata-toolbox-data/output/whisper_log/encoder_all_der_5/slurm_log_%a.txt
-#SBATCH --error=kymata-toolbox-data/output/whisper_log/encoder_all_der_5/slurm_log_%a.txt
+#SBATCH --output=kymata-toolbox-data/output/ru_narr_en_native/language_pilot_01/log/slurm_log_%a.txt
+#SBATCH --error=kymata-toolbox-data/output/ru_narr_en_native/language_pilot_01/log/slurm_log_%a.txt
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=240G
@@ -16,21 +16,25 @@
 #SBATCH --exclusive
 
 # args=(5)
-layer_num=("model.encoder.conv1" "model.encoder.conv2" "model.encoder.layers.0.final_layer_norm" "model.encoder.layers.1.final_layer_norm" "model.encoder.layers.2.final_layer_norm" "model.encoder.layers.3.final_layer_norm" "model.encoder.layers.4.final_layer_norm" "model.encoder.layers.5.final_layer_norm")
+layer_num=("model.encoder.conv1" "model.encoder.conv2" "model.encoder.layers.10.final_layer_norm" "model.encoder.layers.20.final_layer_norm" "model.encoder.layers.30.final_layer_norm" "model.decoder.layers.10.final_layer_norm" "model.decoder.layers.20.final_layer_norm" "model.decoder.layers.30.final_layer_norm")
 # ARG=${args[$SLURM_ARRAY_TASK_ID - 1]}
 
 module load apptainer
 apptainer exec \
+  -B /imaging/woolgar/projects/Tianyi/ \
   -B /imaging/projects/cbu/kymata/ \
   /imaging/local/software/singularity_images/python/python_3.11.7-slim.sif \
   bash -c \
-    " cd /imaging/projects/cbu/kymata/analyses/tianyi/kymata-toolbox/ ; \
+    " cd /imaging/woolgar/projects/Tianyi/kymata-toolbox/ ; \
       export VENV_PATH=~/poetry/ ; \
       export VIRTUAL_ENV=/imaging/woolgar/projects/Tianyi/virtualenvs/kymata-toolbox-jvBImMG9-py3.11/ ; \
       \$VENV_PATH/bin/poetry run python -m invokers.run_gridsearch \
-        --base-dir '/imaging/projects/cbu/kymata/data/dataset_4-english-narratives/' \
-        --function-path 'predicted_function_contours/asr_models/whisper_all_no_reshape' \
+        --config dataset4.1.yaml \
+        --function-path '/imaging/woolgar/projects/Tianyi/data/predicted_function_contours/asr_models/ru_whisper_all_no_reshape_large' \
         --function-name '${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
         --n-derangements 5 \
         --asr-option 'all' \
+        --num-neurons 1280 \
+        --save-plot-location '/imaging/woolgar/projects/Tianyi/kymata-toolbox/kymata-toolbox-data/output/ru_narr_en_native/language_pilot_01/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --save-expression-set-location '/imaging/woolgar/projects/Tianyi/kymata-toolbox/kymata-toolbox-data/output/ru_narr_en_native/language_pilot_01/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
   "
