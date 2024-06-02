6ae2a49ae36a60e26cc9de14a12299501401d751
young-x-skyee <young.x.skyee@outlook.com>
2024-06-02 21:10:29 +0100

    update

--- get_feats_teacher_forcing.py
+++ get_feats_teacher_forcing.py
@@ -1,5 +1,5 @@
 from transformers import AutoProcessor, Wav2Vec2Model
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from transformers import WhisperProcessor, WhisperForConditionalGeneration, WhisperTokenizer
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,13 +16,17 @@ import librosa
 
 start_time = time.time()
 
+test = True
+
 w2v_outs, wavlm_outs, d2v_outs, hubert_outs = False, False, False, False
 whisper_outs = True
 save_outs = True
 
 data_path = '/imaging/projects/cbu/kymata/data/dataset_4-english-narratives'
+# data_path = '/imaging/projects/cbu/kymata/data/dataset_3-russian_narratives'
 
 dataset, sampling_rate = librosa.load(f'{data_path}/stimuli/stimulus.wav', sr=16_000)
+# dataset, sampling_rate = librosa.load(f'{data_path}/stimuli/audio/F00C_dataset3.wav', sr=16_000)
 
 # processor = AutoProcessor.from_pretrained("facebook/wav2vec2-base-960h")
 # inputs = processor(dataset, sampling_rate=sampling_rate, return_tensors="pt")
@@ -32,18 +36,15 @@ T_max = 401 #seconds
 # func_dir = '/imaging/projects/cbu/kymata/data/dataset_4-english-narratives'
 func_dir = '/imaging/woolgar/projects/Tianyi/data'
 
-# func_name = 'whisper_all_no_reshape'
-# func_name = 'whisper_all_no_reshape_small_multi_timestamp'
-func_name = 'whisper_all_no_reshape_tiny_teacher_enforcing'
+func_name = 'whisper_all_no_reshape_large_v2_longform_teacher'
 
 features = {}
 timestamps = []
-text = []
 text_with_time = []
 
 def get_features(name):
   def hook(model, input, output):
-    if isinstance(output,torch.Tensor):
+    if isinstance(output,torch.Tensor) and (('model.decoder.layers' in name and 'final_layer_norm' in name) or 'proj_out' in name):
       if name in features.keys():
         if name == 'model.encoder.conv1' or name == 'model.encoder.conv2':
           # import ipdb;ipdb.set_trace()
@@ -54,20 +55,20 @@ def get_features(name):
         features[name] = output
   return hook
 
-# def get_features(name):
-#   def hook(model, input, output):
-#       features[name] = output
-#   return hook
-
-########
 
 if whisper_outs:
 # if True:
 
   dataset = dataset[:T_max*16_000]
 
-  processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
-  model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+  if test:
+    processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-tiny")
+  else:
+    processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
+    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-large-v2")
   # import ipdb;ipdb.set_trace()
   # for layer in model.children():
   #   layer.register_forward_hook(get_features("feats"))
@@ -77,29 +78,39 @@ if whisper_outs:
     if isinstance(layer, torch.nn.Module):
         layer.register_forward_hook(get_features(name))
 
-  for i in range(14):
-    if i == 13:
-      segment = dataset[i*30*16_000:]
-    else:
-      segment = dataset[i*30*16_000:(i+1)*30*16_000]
-    # inputs = processor(dataset, return_tensors="pt", truncation=False, padding="longest", return_attention_mask=True, sampling_rate=sampling_rate)
-    inputs = processor(segment, sampling_rate=sampling_rate, return_tensors="pt")
+  inputs = processor(dataset, return_tensors="pt", truncation=False, padding="longest", return_attention_mask=True, sampling_rate=sampling_rate)
+  # inputs = processor(segment, sampling_rate=sampling_rate, return_tensors="pt")
+  with open('/imaging/woolgar/projects/Tianyi/kymata-toolbox/kymata-toolbox-data/output/test/transcription_en.txt', 'r') as file:
+    file_content = file.read()
+  labels = tokenizer(file_content, return_tensors="pt")
 
-    import ipdb;ipdb.set_trace()
-    
-    # generated_ids = model.generate(**inputs, return_token_timestamps=True, return_segments=True, return_dict_in_generate=True, num_segment_frames=480_000)
-    generated_ids = model.generate(**inputs, language='english', return_token_timestamps=True, return_segments=True, return_dict_in_generate=True, num_segment_frames=480_000)
-    # generated_ids = model.generate(**inputs, language='english', return_token_timestamps=False, return_segments=True, return_dict_in_generate=True, num_segment_frames=480_000)
-    # import ipdb;ipdb.set_trace()
-    timestamps.append(generated_ids['token_timestamps'].numpy()[:, 1:] + i * 30)
-    text.append(processor.batch_decode(**generated_ids, skip_special_tokens=False)[0])
-    for i in range(generated_ids['sequences'].shape[1]):
-      text_with_time.append(f'{processor.batch_decode(generated_ids["sequences"][:,i], skip_special_tokens=False)[0]}: {generated_ids["token_timestamps"][:,i]}')
-    # transcription = processor.batch_decode(**generated_ids, skip_special_tokens=True)
+  import ipdb;ipdb.set_trace()
+
+  model(input_features=inputs['input_features'], labels=labels['input_ids'])
+  
+  # for i in range(len(generated_ids['segments'][0])):
+  #   timestamps += generated_ids['segments'][0][i]['token_timestamps'].tolist()
+  # timestamps = np.array(timestamps)
+  # text = processor.batch_decode(**generated_ids, skip_special_tokens=False)[0]
+  # # transcription = processor.batch_decode(**generated_ids, skip_special_tokens=True)
+  # for i in range(generated_ids['sequences'].shape[1]):
+  #   text_with_time.append(f'{processor.batch_decode(generated_ids["sequences"][:,i], skip_special_tokens=False)[0]}: {timestamps[i]}')
+
+  # # import ipdb;ipdb.set_trace()
+
+  # text_from_proj_out = processor.batch_decode(torch.argmax(features['proj_out'],dim=2)[0,:],skip_special_tokens=False)
+  # text_from_id = []
+  # for i in range(generated_ids['sequences'].shape[1]):
+  #   text_from_id.append(processor.batch_decode(generated_ids["sequences"][:,i], skip_special_tokens=False)[0])
+  # indices_to_delete = [i for i, ele in enumerate(text_from_proj_out) if ele not in text_from_id]
+  # mask = torch.ones(features['proj_out'].size(1), dtype=torch.bool)
+  # mask[indices_to_delete] = False
+  # for key in features.keys():
+  #   features[key] = features[key][:, mask, :]
 
   end_time = time.time()
   execution_time = end_time - start_time
-  timestamps = np.concatenate(timestamps, axis = 1).reshape(-1)
+
   print(f"Execution time: {execution_time} seconds")
   # import ipdb;ipdb.set_trace()
 
@@ -110,102 +121,6 @@ else:
 
   # import ipdb;ipdb.set_trace()
   
-########
-
-if w2v_outs:
-  model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
-  #model.eval()
-  # model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
-
-  def model_convs(inputs):
-    model_conv_outs = []
-    with torch.no_grad():
-      convs = model.feature_extractor.conv_layers
-      inputs = inputs[:, None]
-      for i, conv in enumerate(convs):
-        inputs = conv(inputs)
-        model_conv_outs.append(np.array(inputs[0]))
-      return model_conv_outs
-
-  conv_outs = model_convs(inputs['input_values'][:, :16_000 * T_max])
-
-########
-
-if wavlm_outs:
-  from transformers import Wav2Vec2FeatureExtractor, WavLMForXVector
-
-  wavlm_model = WavLMForXVector.from_pretrained('microsoft/wavlm-base-plus')
-
-  def wavlm_convs(inputs):
-    model_conv_outs = []
-    with torch.no_grad():
-      convs = wavlm_model.wavlm.feature_extractor.conv_layers
-      inputs = inputs[:, None]
-      for i, conv in enumerate(convs):
-        inputs = conv(inputs)
-        model_conv_outs.append(np.array(inputs[0]))
-      return model_conv_outs
-
-  conv_outs = wavlm_convs(inputs['input_values'][:, :16_000 * T_max])
-
-
-########
-
-if d2v_outs:
-  from transformers import Data2VecAudioForCTC
-
-  d2v_model = Data2VecAudioForCTC.from_pretrained("facebook/data2vec-audio-base")
-
-  def d2v_convs(inputs):
-    model_conv_outs = []
-    with torch.no_grad():
-      convs = d2v_model.data2vec_audio.feature_extractor.conv_layers
-      inputs = inputs[:, None]
-      for i, conv in enumerate(convs):
-        inputs = conv(inputs)
-        model_conv_outs.append(np.array(inputs[0]))
-      return model_conv_outs
-
-  conv_outs = d2v_convs(inputs['input_values'][:, :16_000 * T_max])
-
-
-########
-
-if hubert_outs:
-  from transformers import HubertForCTC
-
-  hubert_model = HubertForCTC.from_pretrained("facebook/hubert-base-ls960")
-
-  def hubert_convs(inputs):
-    model_conv_outs = []
-    with torch.no_grad():
-      convs = hubert_model.hubert.feature_extractor.conv_layers
-      inputs = inputs[:, None]
-      for i, conv in enumerate(convs):
-        inputs = conv(inputs)
-        model_conv_outs.append(np.array(inputs[0]))
-      return model_conv_outs
-
-  conv_outs = hubert_convs(inputs['input_values'][:, :16_000 * T_max])
-
-########
-
-
-if sum((w2v_outs, wavlm_outs, d2v_outs, hubert_outs)) and save_outs:
-  [print(i.shape) for i in conv_outs]
-
-  func_dict = {}
-
-  s_num = T_max * 1000
-
-  # TODO look at latency definitions and egdes
-  for i in range(len(conv_outs)):
-      place_holder = np.zeros((conv_outs[i].shape[0], s_num))
-      for j in range(conv_outs[i].shape[0]):
-          place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, T_max, conv_outs[i].shape[-1]), conv_outs[i][j])
-      func_dict[f'conv_layer{i}'] = place_holder
-
-# import ipdb;ipdb.set_trace()
 
 if whisper_outs and save_outs:
 
@@ -227,48 +142,9 @@ if whisper_outs and save_outs:
     plt.savefig(f'kymata-toolbox-data/output/test/{func_name}_timestamp.png')
     plt.close()
   if not os.path.isfile(f"kymata-toolbox-data/output/test/{func_name}_transcription.txt"):
-    text = "\n".join(text)
     with open(f"kymata-toolbox-data/output/test/{func_name}_transcription.txt", "w") as file:
       file.write(text)
   if not os.path.isfile(f"kymata-toolbox-data/output/test/{func_name}_transcription_time.txt"):
     text_with_time = "\n".join(text_with_time)
     with open(f"kymata-toolbox-data/output/test/{func_name}_transcription_time.txt", "w") as file:
       file.write(text_with_time)  
-
-  # np.savez(f'{func_dir}/predicted_function_contours/asr_models/whisper_all_no_reshape_large_v2.npz', **features)
-
-  # func_dict = {}
-  # for name,val in features.items():
-  #   if 'decoder' in name or name == 'proj_out':
-  #     print(name)
-  #     if 'conv' in name or val.shape[0] != 1:
-  #       place_holder = np.zeros((val.shape[1], s_num))
-  #     else:
-  #       place_holder = np.zeros((val.shape[2], s_num))
-  #     for j in range(place_holder.shape[0]):
-  #       if 'conv' in name:
-  #         place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, 420, val.shape[2]), val[0, j, :])
-  #       elif val.shape[0] != 1:
-  #         place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, 420, val.shape[0]), val[:, j])
-  #       else:
-  #         place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, 420, val.shape[1]), val[0, :, j])
-  #     func_dict[name] = place_holder
-  # np.savez(f'{data_path}/predicted_function_contours/asr_models/whisper_decoder.npz', **func_dict)
-
-  # func_dict = {}
-  # for name,val in features.items():
-  #   if ('decoder' not in name) and ('encoder' in name):
-  #     print(name)
-  #     if 'conv' in name or val.shape[0] != 1:
-  #       place_holder = np.zeros((val.shape[1], s_num))
-  #     else:
-  #       place_holder = np.zeros((val.shape[2], s_num))
-  #     for j in range(place_holder.shape[0]):
-  #       if 'conv' in name:
-  #         place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, 420, val.shape[2]), val[0, j, :])
-  #       elif val.shape[0] != 1:
-  #         place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, 420, val.shape[0]), val[:, j])
-  #       else:
-  #         place_holder[j] = np.interp(np.linspace(0, T_max, s_num + 1)[:-1], np.linspace(0, 420, val.shape[1]), val[0, :, j])
-  #     func_dict[name] = place_holder
-  # np.savez(f'{data_path}/predicted_function_contours/asr_models/whisper_encoder.npz', **func_dict)
