c26aa221bc1d5c6fcc56e588b8e037bdcadd3a0c
young-x-skyee <young.x.skyee@outlook.com>
2024-07-10 11:20:48 +0100

    update

--- submit_gridsearch.sh
+++ submit_gridsearch.sh
@@ -24,16 +24,20 @@ apptainer exec \
   -B /imaging/projects/cbu/kymata/ \
   /imaging/local/software/singularity_images/python/python_3.11.7-slim.sif \
   bash -c \
-    " cd /imaging/woolgar/projects/Tianyi/kymata-toolbox/ ; \
+    " cd /imaging/projects/cbu/kymata/analyses/tianyi/kymata-core ; \
       export VENV_PATH=~/poetry/ ; \
-      export HF_HOME=/imaging/woolgar/projects/Tianyi/models ; \
-      export VIRTUAL_ENV=/imaging/woolgar/projects/Tianyi/virtualenvs/kymata-toolbox-jvBImMG9-py3.11/ ; \
+      export VIRTUAL_ENV=/imaging/woolgar/projects/Tianyi/virtualenvs/kymata-toolbox-jvBImMG9-py3.11/
       \$VENV_PATH/bin/poetry run python -m invokers.run_gridsearch \
         --config dataset4.yaml \
         --input-stream auditory \
         --function-path 'predicted_function_contours/GMSloudness/stimulisig' \
+        --use-inverse-operator \
+        --inverse-operator-suffix '_ico5-3L-loose02-cps-nodepth-fusion-diag-inv.fif' \
         --function-name IL STL IL1 IL2 IL3 IL4 IL5 IL6 IL7 IL8 IL9  \
-        --overwrite
+        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/diag_only' \
+        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/diag_only' \
+        --overwrite \
+        --morph \
   "
   #  --snr $ARG # >> result3.txt
         # --single-participant-override 'participant_01' \
@@ -41,3 +45,6 @@ apptainer exec \
         # --inverse-operator-suffix '_ico5-3L-loose02-cps-nodepth-megonly-emptyroom1-inv.fif'
   
   #  --function-path '/imaging/projects/cbu/kymata/data/open-source/ERP_CORE/MMN All Data and Scripts/functions/kymata_mr' \
+
+      # export HF_HOME=/imaging/woolgar/projects/Tianyi/models ; \
+      # export VIRTUAL_ENV=/imaging/woolgar/projects/Tianyi/virtualenvs/kymata-toolbox-jvBImMG9-py3.11/ ; \
