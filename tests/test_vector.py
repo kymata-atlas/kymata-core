c0a81ab3d558d6f52d5dce3a8adbc17cd7fc1791
Cai Wingfield <cai@caiwingfield.net>
2024-07-27 09:08:47 +0100

    (Now the test actually fails)

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -7,7 +7,7 @@ from kymata.math.vector import normalize
 @pytest.fixture
 def small_vector():
     return array([0.0001237, 0.0001237, 0.0001237, 0.0001237, 0.0001237, 0.0001237,
-                  0.0001237, 0.0001237, 0.0001237, 0.0001237], dtype=float16)
+                  0.0001237, 0.0001237, 0.0001236, 0.0001237], dtype=float16)
 
 
 def test_normalize_small_vector(small_vector):
