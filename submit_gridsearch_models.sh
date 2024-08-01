8258467c0541747004fd901bca185eb22875e993
young-x-skyee <young.x.skyee@outlook.com>
2024-08-01 08:20:29 +0100

    update

--- submit_gridsearch_models.sh
+++ submit_gridsearch_models.sh
@@ -11,13 +11,18 @@
 #SBATCH --error=kymata-core-data/output/ru_narr_en_native/language_pilots_all/log/slurm_log_%a.txt
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=240G
-#SBATCH --array=0-7
+#SBATCH --mem=10G
+#SBATCH --array=0-65
 #SBATCH --exclusive
 
-# args=(5)
-layer_num=("model.encoder.conv1" "model.encoder.conv2" "model.encoder.layers.10.final_layer_norm" "model.encoder.layers.20.final_layer_norm" "model.encoder.layers.30.final_layer_norm" "model.decoder.layers.10.final_layer_norm" "model.decoder.layers.20.final_layer_norm" "model.decoder.layers.30.final_layer_norm")
+layer_num=("model.encoder.conv1" "model.encoder.conv2")
 # ARG=${args[$SLURM_ARRAY_TASK_ID - 1]}
+for ((i=0; i<32; i++)); do
+    layer_num+=("model.encoder.layers.$i.final_layer_norm")
+done
+for ((i=0; i<32; i++)); do
+    layer_num+=("model.decoder.layers.$i.final_layer_norm")
+done
 
 module load apptainer
 apptainer exec \
@@ -30,6 +35,7 @@ apptainer exec \
       export VIRTUAL_ENV=/imaging/woolgar/projects/Tianyi/virtualenvs/kymata-toolbox-jvBImMG9-py3.11/ ; \
       \$VENV_PATH/bin/poetry run python -m invokers.run_gridsearch \
         --config dataset4.1.yaml \
+        --plot-top-channels \
         --input-stream auditory \
         --function-path '/imaging/woolgar/projects/Tianyi/data/predicted_function_contours/asr_models/ru_whisper_all_no_reshape_large' \
         --function-name '${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
