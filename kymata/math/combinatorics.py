91f3fed7e31a497ab93ba2c466255a3ba8219614
neukym <acgt2@cam.ac.uk>
2024-08-04 21:23:24 +0100

    Update combinatorics.py

--- kymata/math/combinatorics.py
+++ kymata/math/combinatorics.py
@@ -2,7 +2,7 @@ from numpy import arange
 from numpy.random import randint
 
 
-def generate_derangement(n, mod=int(1e9)):  # approx 3ms runtime for n=400
+def generate_derangement(n, mod=int(1e9)):  # approx 3ms runtime for n=400 
     while True:
         v = arange(n)
         for j in range(n - 1, -1, -1):
