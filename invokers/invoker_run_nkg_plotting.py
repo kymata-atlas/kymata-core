6df3c1701bec57ce2bd92afd5d51b40f7f4f7c3e
young-x-skyee <young.x.skyee@outlook.com>
2024-08-06 11:05:43 +0100

    update

--- invokers/invoker_run_nkg_plotting.py
+++ invokers/invoker_run_nkg_plotting.py
@@ -7,6 +7,9 @@ from kymata.io.nkg import load_expression_set
 from kymata.plot.plot import expression_plot, legend_display_dict
 from kymata.plot.color import constant_color_dict, gradient_color_dict
 
+import time
+from tqdm import tqdm
+
 def main():
     function_family_type = 'ANN' # 'standard' or 'ANN' or 'simple'
     path_to_nkg_files = Path(Path(path.abspath("")).parent, "kymata-core", "kymata-core-data", "output")
@@ -36,81 +39,95 @@ def main():
 
     elif function_family_type == 'ANN':
 
-        path_to_nkg_files = Path(path_to_nkg_files, 'whisper/encoder_all_der_5')
+        start = time.time()
 
-        expression_data = load_expression_set(Path( path_to_nkg_files, "model.encoder.conv1_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.conv2_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.layers.0.final_layer_norm_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.layers.1.final_layer_norm_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.layers.2.final_layer_norm_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.layers.3.final_layer_norm_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.layers.4.final_layer_norm_511_gridsearch.nkg"))
-        expression_data += load_expression_set(Path( path_to_nkg_files, "model.encoder.layers.5.final_layer_norm_511_gridsearch.nkg"))
+        path_to_nkg_files = Path(path_to_nkg_files, 'whisper_large_multi')
 
-        conv1_list = []
-        conv2_list = []
-        encoder0_list = []
-        encoder1_list = []
-        encoder2_list = []
-        encoder3_list = []
-        encoder4_list = []
-        encoder5_list = []
+        # Function to find all files ending with _gridsearch.nkg
+        def find_nkg_files(directory):
+            return list(directory.glob('**/*_gridsearch.nkg'))
 
-        # Loop through the range from 0 to 511
-        for i in range(512):
-            conv1 = f"model.encoder.conv1_{i}"
-            conv1_list.append(conv1)
+        # Find all matching files in the specified directories
+        nkg_files = find_nkg_files(path_to_nkg_files)
 
-        for i in range(512):
-            conv2 = f"model.encoder.conv2_{i}"
-            conv2_list.append(conv2)
+        # Initialize expression_data
+        expression_data = None
 
-        for i in range(512):
-            encoder0 = f"model.encoder.layers.0.final_layer_norm_{i}"
-            encoder0_list.append(encoder0)
+        # Loop through the file paths and load the data
+        for file_path in tqdm(nkg_files):
+            data = load_expression_set(file_path)
+            if expression_data is None:
+                expression_data = data
+            else:
+                expression_data += data
 
-        for i in range(512):
-            encoder1 = f"model.encoder.layers.1.final_layer_norm_{i}"
-            encoder1_list.append(encoder1)
+        # conv1_list = []
+        # conv2_list = []
+        # encoder0_list = []
+        # encoder1_list = []
+        # encoder2_list = []
+        # encoder3_list = []
+        # encoder4_list = []
+        # encoder5_list = []
 
-        for i in range(512):
-            encoder2 = f"model.encoder.layers.2.final_layer_norm_{i}"
-            encoder2_list.append(encoder2)
+        # # Loop through the range from 0 to 511
+        # for i in range(512):
+        #     conv1 = f"model.encoder.conv1_{i}"
+        #     conv1_list.append(conv1)
 
-        for i in range(512):
-            encoder3 = f"model.encoder.layers.3.final_layer_norm_{i}"
-            encoder3_list.append(encoder3)
+        # for i in range(512):
+        #     conv2 = f"model.encoder.conv2_{i}"
+        #     conv2_list.append(conv2)
 
-        for i in range(512):
-            encoder4 = f"model.encoder.layers.4.final_layer_norm_{i}"
-            encoder4_list.append(encoder4)
+        # for i in range(512):
+        #     encoder0 = f"model.encoder.layers.0.final_layer_norm_{i}"
+        #     encoder0_list.append(encoder0)
 
-        for i in range(512):
-            encoder5 = f"model.encoder.layers.5.final_layer_norm_{i}"
-            encoder5_list.append(encoder5)
+        # for i in range(512):
+        #     encoder1 = f"model.encoder.layers.1.final_layer_norm_{i}"
+        #     encoder1_list.append(encoder1)
+
+        # for i in range(512):
+        #     encoder2 = f"model.encoder.layers.2.final_layer_norm_{i}"
+        #     encoder2_list.append(encoder2)
+
+        # for i in range(512):
+        #     encoder3 = f"model.encoder.layers.3.final_layer_norm_{i}"
+        #     encoder3_list.append(encoder3)
+
+        # for i in range(512):
+        #     encoder4 = f"model.encoder.layers.4.final_layer_norm_{i}"
+        #     encoder4_list.append(encoder4)
+
+        # for i in range(512):
+        #     encoder5 = f"model.encoder.layers.5.final_layer_norm_{i}"
+        #     encoder5_list.append(encoder5)
 
         expression_plot(expression_data,
                         ylim=-400,
                         xlims=(-200, 800),
-                        save_to=Path(Path(path.abspath("")).parent, "kymata-core/kymata-core-data", "output/encoder_all.jpg"),
-                        show_legend=True,
-                        color= constant_color_dict(conv1_list, color='red')
-                                | constant_color_dict(conv2_list, color='green')
-                                | constant_color_dict(encoder0_list, color='blue')
-                                | constant_color_dict(encoder1_list, color='cyan')
-                                | constant_color_dict(encoder2_list, color='magenta')
-                                | constant_color_dict(encoder3_list, color='yellow')
-                                | constant_color_dict(encoder4_list, color='orange')
-                                | constant_color_dict(encoder5_list, color='purple'),
-                        legend_display=legend_display_dict(conv1_list, 'Conv layer 1')
-                                       | legend_display_dict(conv1_list, 'Conv layer 1')
-                                       | legend_display_dict(conv2_list, 'Conv layer 2')
-                                       | legend_display_dict(encoder0_list, 'Encoder layer 1')
-                                       | legend_display_dict(encoder1_list, 'Encoder layer 2')
-                                       | legend_display_dict(encoder2_list, 'Encoder layer 3')
-                                       | legend_display_dict(encoder3_list, 'Encoder layer 4')
-                                       | legend_display_dict(encoder4_list, 'Encoder layer 5')
-                                       | legend_display_dict(encoder5_list, 'Encoder layer 6'))
+                        save_to=Path(Path(path.abspath("")).parent, "kymata-core/kymata-core-data", "output/whisper_large_all_expression.jpg"),
+                        show_legend=False,)
+                        # color= constant_color_dict(conv1_list, color='red')
+                        #         | constant_color_dict(conv2_list, color='green')
+                        #         | constant_color_dict(encoder0_list, color='blue')
+                        #         | constant_color_dict(encoder1_list, color='cyan')
+                        #         | constant_color_dict(encoder2_list, color='magenta')
+                        #         | constant_color_dict(encoder3_list, color='yellow')
+                        #         | constant_color_dict(encoder4_list, color='orange')
+                        #         | constant_color_dict(encoder5_list, color='purple'),
+                        # legend_display=legend_display_dict(conv1_list, 'Conv layer 1')
+                        #                | legend_display_dict(conv1_list, 'Conv layer 1')
+                        #                | legend_display_dict(conv2_list, 'Conv layer 2')
+                        #                | legend_display_dict(encoder0_list, 'Encoder layer 1')
+                        #                | legend_display_dict(encoder1_list, 'Encoder layer 2')
+                        #                | legend_display_dict(encoder2_list, 'Encoder layer 3')
+                        #                | legend_display_dict(encoder3_list, 'Encoder layer 4')
+                        #                | legend_display_dict(encoder4_list, 'Encoder layer 5')
+                        #                | legend_display_dict(encoder5_list, 'Encoder layer 6'))
+
+        total_time_in_seconds = time.time() - start
+        print(f'Time taken for code to run: {time.strftime("%H:%M:%S", time.gmtime(total_time_in_seconds))} ({total_time_in_seconds:.4f}s)')
 
 if __name__ == '__main__':
     basicConfig(format=log_message, datefmt=date_format, level=INFO)
