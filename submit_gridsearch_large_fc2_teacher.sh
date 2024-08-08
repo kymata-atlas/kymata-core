815c9e5b07a5f1c80e88a3966d78bcb3d638f26f
young-x-skyee <young.x.skyee@outlook.com>
2024-08-08 22:26:07 +0100

    update

--- submit_gridsearch_large_fc2_teacher.sh
+++ submit_gridsearch_large_fc2_teacher.sh
@@ -2,17 +2,17 @@
 
 ###
 # To run gridsearch on the queue at the CBU, run the following command in command line:
-#   sbatch submit_gridsearch_models_fc2.sh
+#   sbatch submit_gridsearch_large_fc2_teacher.sh
 ###
 
 
 #SBATCH --job-name=gridsearch
-#SBATCH --output=kymata-core-data/output/fc2_test/mfa/log/slurm_log_%a.txt
-#SBATCH --error=kymata-core-data/output/fc2_test/mfa/log/slurm_log_%a.txt
+#SBATCH --output=kymata-core-data/output/paper/large/fc2/log/slurm_log_%a.txt
+#SBATCH --error=kymata-core-data/output/paper/large/fc2/log/slurm_log_%a.txt
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=10G
-#SBATCH --array=0-31
+#SBATCH --array=0-63
 #SBATCH --exclusive
 
 # args=(5)
@@ -20,6 +20,10 @@ layer_num=()
 # ARG=${args[$SLURM_ARRAY_TASK_ID - 1]}
 for ((i=0; i<32; i++)); do
     layer_num+=("model.encoder.layers.$i.fc2")
+done
+
+for ((i=0; i<32; i++)); do
+    layer_num+=("model.decoder.layers.$i.fc2")
 done
 
 module load apptainer
@@ -34,11 +38,12 @@ apptainer exec \
       \$VENV_PATH/bin/poetry run python -m invokers.run_gridsearch \
         --config dataset4.yaml \
         --input-stream auditory \
-        --function-path '/imaging/woolgar/projects/Tianyi/data/predicted_function_contours/asr_models/whisper_all_no_reshape_large_multi' \
+        --function-path '/imaging/projects/cbu/kymata/data/dataset_4-english_narratives/predicted_function_contours/asr_models/whisper_fc2_and_final_layer_norm/whisper_large_teacher' \
+        --num-neurons 1280 \
         --function-name '${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
         --n-derangements 5 \
         --asr-option 'all' \
-        --num-neurons 1280 \
-        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/fc2_test/mfa/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
-        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/fc2_test/mfa/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --mfa True \
+        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/large/fc2/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/large/fc2/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
   "
