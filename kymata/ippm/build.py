765c48efed8c4e047b1af6e4bb4278ab17f5871e
Cai Wingfield <cai@caiwingfield.net>
2024-08-17 18:47:29 +0100

    Make public

--- kymata/ippm/build.py
+++ kymata/ippm/build.py
@@ -23,7 +23,8 @@ class IPPMBuilder:
         self._hierarchy = deepcopy(hierarchy)
         self._hemisphere: str = hemisphere
 
-        self.graph: IPPMGraph = self._build_graph_dict()
+        self.graph: IPPMGraph = dict()
+        self.graph = self._build_graph_dict()
 
     def _build_graph_dict(self) -> IPPMGraph:
         self._sort_spikes_by_latency_asc()
@@ -34,13 +35,13 @@ class IPPMBuilder:
         partition_ptr = 0
         while childless_functions := self._get_childless_functions():
             for childless_func in childless_functions:
-                self._graph = self._create_nodes_and_edges_for_function(
+                self.graph = self._create_nodes_and_edges_for_function(
                     childless_func, partition_ptr, y_axis_partition_size
                 )
                 self._hierarchy.pop(childless_func)
                 partition_ptr += 1
 
-        return self._graph
+        return self.graph
 
     def _sort_spikes_by_latency_asc(self) -> None:
         for function in self._spikes.keys():
@@ -69,23 +70,23 @@ class IPPMBuilder:
         func_parents = self._hierarchy[function_name]
         current_y_axis_coord = __get_y_coordinate(partition_ptr, partition_size)
         if function_name in self._inputs:
-            self._graph[function_name] = IPPMNode(100, (0, current_y_axis_coord), [])
+            self.graph[function_name] = IPPMNode(100, (0, current_y_axis_coord), [])
         else:
             childless_func_pairings = self._get_best_pairings_from_hemisphere(
                 function_name
             )
 
             if len(childless_func_pairings) == 0:
-                return self._graph
+                return self.graph
 
-            self._graph = self._create_nodes_for_childless_function(
+            self.graph = self._create_nodes_for_childless_function(
                 current_y_axis_coord, childless_func_pairings, function_name
             )
-            self._graph = self._create_edges_between_parents_and_childless_function(
+            self.graph = self._create_edges_between_parents_and_childless_function(
                 func_parents, function_name
             )
 
-        return self._graph
+        return self.graph
 
     def _get_best_pairings_from_hemisphere(
         self, func: str
@@ -110,13 +111,13 @@ class IPPMBuilder:
         for idx, pair in enumerate(childless_func_pairings):
             latency, magnitude = pair
             parent_function = [function_name + "-" + str(idx - 1)] if idx != 0 else []
-            self._graph[function_name + "-" + str(idx)] = IPPMNode(
+            self.graph[function_name + "-" + str(idx)] = IPPMNode(
                 __map_magnitude_to_node_size(magnitude),
                 (latency, current_y_axis_coord),
                 parent_function,
             )
 
-        return self._graph
+        return self.graph
 
     def _create_edges_between_parents_and_childless_function(
         self,
@@ -126,12 +127,12 @@ class IPPMBuilder:
         if function_name in self._spikes.keys():
             for parent in parents:
                 if parent in self._inputs:
-                    self._graph[function_name + "-0"].inc_edges.append(parent)
+                    self.graph[function_name + "-0"].inc_edges.append(parent)
                 else:
                     parent_pairings = self._get_best_pairings_from_hemisphere(parent)
                     if len(parent_pairings) > 0:
-                        self._graph[function_name + "-0"].inc_edges.append(
+                        self.graph[function_name + "-0"].inc_edges.append(
                             parent + "-" + str(len(parent_pairings) - 1)
                         )
 
-        return self._graph
+        return self.graph
