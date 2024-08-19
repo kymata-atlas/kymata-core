06c7d69b4bcb3fe42927dbb773b845086774d377
young-x-skyee <young.x.skyee@outlook.com>
2024-08-19 16:15:27 +0100

    update

--- submit_gridsearch_salmonn_13b_teacher.sh
+++ submit_gridsearch_salmonn_13b_teacher.sh
@@ -2,23 +2,23 @@
 
 ###
 # To run gridsearch on the queue at the CBU, run the following command in command line:
-#   sbatch submit_gridsearch_salmonn_7b_teacher.sh
+#   sbatch submit_gridsearch_salmonn_13b_teacher.sh
 ###
 
 
 #SBATCH --job-name=gridsearch
-#SBATCH --output=kymata-core-data/output/paper/salmonn_7b/fc2/log/slurm_log_%a.txt
-#SBATCH --error=kymata-core-data/output/paper/salmonn_7b/fc2/log/slurm_log_%a.txt
+#SBATCH --output=kymata-core-data/output/paper/salmonn_13b/fc2/log/slurm_log_%a.txt
+#SBATCH --error=kymata-core-data/output/paper/salmonn_13b/fc2/log/slurm_log_%a.txt
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=10G
-#SBATCH --array=0-32
+#SBATCH --array=0-40
 #SBATCH --exclusive
 
 # args=(5)
 layer_num=()
 # ARG=${args[$SLURM_ARRAY_TASK_ID - 1]}
-for ((i=0; i<33; i++)); do
+for ((i=0; i<41; i++)); do
     layer_num+=("layer$i")
 done
 
@@ -35,12 +35,12 @@ apptainer exec \
         --config dataset4.yaml \
         --input-stream auditory \
         --plot-top-channels \
-        --function-path '/imaging/projects/cbu/kymata/data/dataset_4-english_narratives/predicted_function_contours/asr_models/salmonn/7B' \
-        --num-neurons 4096 \
+        --function-path '/imaging/projects/cbu/kymata/data/dataset_4-english_narratives/predicted_function_contours/asr_models/salmonn/13B' \
+        --num-neurons 5120 \
         --function-name '${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
         --n-derangements 5 \
         --asr-option 'all' \
         --mfa True \
-        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/salmonn_7b/fc2/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
-        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/salmonn_7b/fc2/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/salmonn_13b/fc2/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/salmonn_13b/fc2/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
   "
