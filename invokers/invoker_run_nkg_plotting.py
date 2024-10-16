c6c7e223eb7d2149bd05cafd26a18e12b9f11b63
young-x-skyee <young.x.skyee@outlook.com>
2024-10-16 17:22:43 +0100

    update

--- invokers/invoker_run_nkg_plotting.py
+++ invokers/invoker_run_nkg_plotting.py
@@ -65,8 +65,8 @@ def main():
         expression_data_syntax = load_expression_set('/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/feats/source/syntax/expression_set/syntax_4_gridsearch.nkg')
         syntax_name = expression_data_syntax.functions
 
-        # all_data = expression_data_salmonn + expression_data_word + expression_data_syntax
-        all_data = expression_data_word + expression_data_syntax
+        all_data = expression_data_salmonn + expression_data_word + expression_data_syntax
+        # all_data = expression_data_word + expression_data_syntax
 
         # import ipdb;ipdb.set_trace()
 
@@ -81,15 +81,15 @@ def main():
         # fig = expression_plot(expression_data, paired_axes=True, minimap=False, show_legend=True,
                             #   | constant_color_dict(phonetic_func, 'green'))
 
-        # fig = expression_plot(all_data, paired_axes=True, minimap=False, show_legend=True,
-        #                         color= constant_color_dict(word_name+syntax_name, color='green')
-        #                             | constant_color_dict(salmonn_name, color= 'red'),
-        #                         legend_display=legend_display_dict(word_name+syntax_name, 'Word features')
-        #                             | legend_display_dict(salmonn_name, 'SALMONN neurons'))
-        fig = expression_plot(all_data, paired_axes=True, minimap=False, show_legend=True)
+        fig = expression_plot(all_data, paired_axes=True, minimap=False, show_legend=True,
+                                color= constant_color_dict(word_name+syntax_name, color='green')
+                                    | constant_color_dict(salmonn_name, color= 'red'),
+                                legend_display=legend_display_dict(word_name+syntax_name, 'Word features')
+                                    | legend_display_dict(salmonn_name, 'SALMONN neurons'))
+        # fig = expression_plot(all_data, paired_axes=True, minimap=True, show_legend=True)
 
 
-        fig.savefig("/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/feats_source.png")
+        fig.savefig("/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/salmonn_vs_feats_source.png")
 
     elif function_family_type == 'standard':
 
