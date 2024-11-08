769795975f3aa8c48da6d30a4a4787df17f09488
Cai Wingfield <cai@caiwingfield.net>
2024-11-08 13:12:35 +0000

    Add some failing tests to define function behaviour

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -464,3 +464,177 @@ def test_subset_transforms_two():
     )
 
     assert es["first", "third"] == first_two
+
+
+def test_time_crop_both_endpoints_inside():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data_left  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+    data_right = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=latencies,
+        data_lh=data_left,
+        data_rh=data_right,
+    )
+
+    cropped = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=[-.2, -.1, 0, .1, .2, .3],
+        data_lh=[d[:, 2:8] for d in data_left],
+        data_rh=[d[:, 2:8] for d in data_right],
+    )
+
+    # Inclusive of endpoints
+    assert es.crop_time(-.2, .3) == cropped
+
+    # Endpoints between timepoints
+    assert es.crop_time(-.25, .35) == cropped
+
+
+def test_time_crop_half_open_left():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data_left  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+    data_right = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=latencies,
+        data_lh=data_left,
+        data_rh=data_right,
+    )
+
+    cropped = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=[-.5, -.4, -.3, -.2, -.1],
+        data_lh=[d[:, :4] for d in data_left],
+        data_rh=[d[:, :4] for d in data_right],
+    )
+
+    # Inclusive of endpoints
+    assert es.crop_time(None, -.1) == cropped
+
+    # Endpoints between timepoints
+    assert es.crop_time(None, -.05) == cropped
+
+
+def test_time_crop_half_open_right():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data_left  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+    data_right = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=latencies,
+        data_lh=data_left,
+        data_rh=data_right,
+    )
+
+    cropped = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=[-.1, 0, .1, .2, .3, .4, .5],
+        data_lh=[d[:, 4:] for d in data_left],
+        data_rh=[d[:, 4:] for d in data_right],
+    )
+
+    # Inclusive of endpoints
+    assert es.crop_time(-.1, None) == cropped
+
+    # Endpoints between timepoints
+    assert es.crop_time(-.15, None) == cropped
+
+
+def test_time_crop_contains_whole():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data_left  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+    data_right = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=latencies,
+        data_lh=data_left,
+        data_rh=data_right,
+    )
+
+    assert es.crop_time(-1, 1) == es
+
+
+def test_time_crop_one_datapoint_at_end():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = SensorExpressionSet(
+        transforms=["first", "second", "third"],
+        sensors=range(5),
+        latencies=latencies,
+        data=data,
+    )
+
+    cropped = SensorExpressionSet(
+        transforms=["first", "second", "third"],
+        sensors=range(5),
+        latencies=[.5],
+        data=[d[:, -1] for d in data],
+    )
+
+    assert es.crop_time(.401, 1) == cropped
+
+
+def test_time_crop_no_datapoints_between():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data_left  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+    data_right = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=latencies,
+        data_lh=data_left,
+        data_rh=data_right,
+    )
+
+    with pytest.raises(IndexError):
+        es.crop_time(0.01, 0.02)
+
+
+def test_time_crop_outside_range():
+    #              0    1    2    3    4  5   6   7   8   9  10
+    latencies = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+    data_left  = [np.random.randn(5, len(latencies)) for _ in range(3)]
+    data_right = [np.random.randn(5, len(latencies)) for _ in range(3)]
+
+    es = HexelExpressionSet(
+        transforms=["first", "second", "third"],
+        hexels_lh=range(5),
+        hexels_rh=range(5),
+        latencies=latencies,
+        data_lh=data_left,
+        data_rh=data_right,
+    )
+
+    with pytest.raises(IndexError):
+        es.crop_time(-1, -.8)
+
+    with pytest.raises(IndexError):
+        es.crop_time(1, 2)
