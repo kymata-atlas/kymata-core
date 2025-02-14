7338afc396af04f402ece6572d460d3533cc22b0
Cai Wingfield <cai@caiwingfield.net>
2025-02-14 10:46:48 +0000

    Text bugfixes

--- tests/test_ippm_denoising_strategies_integration_tests.py
+++ tests/test_ippm_denoising_strategies_integration_tests.py
@@ -4,6 +4,7 @@ from kymata.entities.constants import HEMI_RIGHT
 from kymata.entities.expression import ExpressionPoint
 from kymata.ippm.denoising_strategies import (
     MaxPoolingStrategy, AdaptiveMaxPoolingStrategy, GMMStrategy, DBSCANStrategy, MeanShiftStrategy)
+from kymata.math.probability import p_to_logp, sidak_correct, p_threshold_for_sigmas
 
 n_timepoints = 201
 n_hexels = 200_000
@@ -66,6 +67,8 @@ noisy_test_hexels = {
     "trans2":  test_data_trans2,
 }
 
+threshold_logp = p_to_logp(sidak_correct(p_threshold_for_sigmas(5), n_comparisons=200 * 200_000))
+
 
 # NOTE: Max Pooling is set to true in the first integ test only. IF we set it to true, then they all do same thing.
 #       Which means we can't see how well it works without the max pooling
@@ -75,8 +78,8 @@ noisy_test_hexels = {
 
 def test_MaxPoolingStrategy_AllTrue_Fit_Successfully():
     expected_denoised = deepcopy(noisy_test_hexels)
-    expected_denoised["trans1"] = [ExpressionPoint("c", 30, "f", 1e-100)]
-    expected_denoised["trans2"] = [ExpressionPoint("c", 30, "f", 1e-99)]
+    expected_denoised["trans1"] = [ExpressionPoint("c", 30, "trans1", -100)]
+    expected_denoised["trans2"] = [ExpressionPoint("c", 30, "trans2", -99)]
 
     strategy = MaxPoolingStrategy(
         should_normalise=True,
@@ -84,7 +87,7 @@ def test_MaxPoolingStrategy_AllTrue_Fit_Successfully():
         should_max_pool=True,
         bin_significance_threshold=2,
     )
-    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, None)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -105,7 +108,7 @@ def test_MaxPoolingStrategy_AllDefault_Fit_Successfully():
     expected_denoised["trans2"] = [("c", 30, "trans2", -99)]
 
     strategy = MaxPoolingStrategy(HEMI_RIGHT)
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -123,13 +126,11 @@ def test_AdaptiveMaxPoolingStrategy_AllTrue_Fit_Successfully():
     expected_denoised["trans2"] = [("c", 30, "trans2", -99)]
 
     strategy = AdaptiveMaxPoolingStrategy(
-        hemi=HEMI_RIGHT,
-        n_timepoints=n_timepoints, n_hexels=n_hexels,
         should_normalise=True,
         should_cluster_only_latency=True,
         bin_significance_threshold=2,
     )
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -151,11 +152,9 @@ def test_AdaptiveMaxPoolingStrategy_AllDefault_Fit_Successfully():
     expected_denoised["trans2"] = [ExpressionPoint("c", 30, "trans2", -99), ExpressionPoint("c", 130, "trans2", -81)]
 
     strategy = AdaptiveMaxPoolingStrategy(
-        HEMI_RIGHT,
-        n_timepoints=n_timepoints, n_hexels=n_hexels,
         bin_significance_threshold=2, base_bin_size=0.025
     )
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -179,14 +178,12 @@ def test_GMMStrategy_AllTrue_Fit_Successfully():
     expected_denoised["trans2"] = [ExpressionPoint("c", 30, "trans2", -99), ExpressionPoint("c", 130, "trans2", -81)]
 
     strategy = GMMStrategy(
-        HEMI_RIGHT,
-        n_timepoints=n_timepoints, n_hexels=n_hexels,
         should_normalise=True,
         should_cluster_only_latency=True,
         number_of_clusters_upper_bound=5,
         random_state=random_seed,
     )
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -212,10 +209,8 @@ def test_GMMStrategy_AllDefault_Fit_Successfully():
         ExpressionPoint("c", 130, "trans2", -81),
     ]
 
-    strategy = GMMStrategy(HEMI_RIGHT,
-                           n_timepoints=n_timepoints, n_hexels=n_hexels,
-                           number_of_clusters_upper_bound=5, random_state=random_seed, should_evaluate_using_aic=False)
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    strategy = GMMStrategy(number_of_clusters_upper_bound=5, random_state=random_seed, should_evaluate_using_aic=False)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -237,13 +232,11 @@ def test_DBSCANStrategy_AllTrue_Fit_Successfully():
     expected_denoised["trans2"] = [ExpressionPoint("c", 30, "trans2", -99), ExpressionPoint("c", 130, "trans2", -81)]
 
     strategy = DBSCANStrategy(
-        HEMI_RIGHT,
-        n_timepoints=n_timepoints, n_hexels=n_hexels,
         should_normalise=False,
         should_cluster_only_latency=True,
         eps=25,
     )
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -265,8 +258,8 @@ def test_DBSCANStrategy_AllDefault_Fit_Successfully():
     ]
     expected_denoised["trans2"] = [ExpressionPoint("c", 30, "trans2", -99), ExpressionPoint("c", 130, "trans2", -81)]
 
-    strategy = DBSCANStrategy(HEMI_RIGHT, n_timepoints=n_timepoints, n_hexels=n_hexels)
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    strategy = DBSCANStrategy()
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -288,11 +281,9 @@ def test_MeanShiftStrategy_AllTrue_Fit_Successfully():
     expected_denoised["trans2"] = [ExpressionPoint("c", 130, "trans2", -81), ExpressionPoint("c", 30, "trans2", -99)]
 
     strategy = MeanShiftStrategy(
-        HEMI_RIGHT,
-        n_timepoints=n_timepoints, n_hexels=n_hexels,
         bandwidth=0.03, min_bin_freq=2
     )
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
@@ -313,10 +304,8 @@ def test_MeanShiftStrategy_AllDefault_Fit_Successfully():
     ]
     expected_denoised["trans2"] = [ExpressionPoint("c", 130, "trans2", -81), ExpressionPoint("c", 30, "trans2", -99)]
 
-    strategy = MeanShiftStrategy(HEMI_RIGHT,
-                                 n_timepoints=n_timepoints, n_hexels=n_hexels,
-                                 bandwidth=0.03, min_bin_freq=2)
-    actual_denoised = strategy.denoise(noisy_test_hexels)
+    strategy = MeanShiftStrategy(bandwidth=0.03, min_bin_freq=2)
+    actual_denoised = strategy._denoise_spikes(noisy_test_hexels, threshold_logp)
 
     assert (
         set(actual_denoised["trans1"])
