779c9a4b6b1294ffac284d134f308ca6b2257066
young-x-skyee <young.x.skyee@outlook.com>
2025-02-18 22:34:49 +0000

    update

--- combine_scatter_all.py
+++ combine_scatter_all.py
@@ -9,7 +9,9 @@ import matplotlib.colors as mcolors
 def list_files_in_directory(directory):
     try:
         files = os.listdir(directory)
-        return [file for file in files if os.path.isfile(os.path.join(directory, file))]
+        files = [file for file in files if os.path.isfile(os.path.join(directory, file))]
+        files.sort(key=lambda f: int(re.search(r'\d+', f).group()))
+        return files
     except Exception as e:
         print(f"An error occurred: {e}")
         return []
@@ -22,7 +24,7 @@ def asr_models_loop_full():
 
     # thres = 20
     alpha = 1 - NormalDist(mu=0, sigma=1).cdf(5)
-    thres = - np.log10(1 - ((1 - alpha)** (np.float128(1 / (2*200*370*neuron*layer)))))
+    thres = - np.log10(1 - ((1 - alpha)** (np.float128(1 / (200*370*neuron*layer)))))
 
     size = 'large'
 
@@ -33,7 +35,8 @@ def asr_models_loop_full():
     lat_sig = np.zeros((n, layer, neuron, 6)) # ( model, layer, neuron, (peak lat, peak corr, ind, -log(pval), layer_no, neuron_no) )
     
     # log_dir_1 = '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/ru_en/all_pilots/log/'
-    log_dir_1 = '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/size/large-v2/fc2/log/'
+    log_dir_1 = '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/ccn_paper/ru/log/'
+    # log_dir_1 = '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/ccn_paper/ru_en/all_pilots/log/'
     file_list = list_files_in_directory(log_dir_1)
 
     # log_dir_2 = f'/imaging/woolgar/projects/Tianyi/kymata-toolbox/kymata-toolbox-data/output/whisper_{size}_multi_log/decoder_all_der_5/'
@@ -51,7 +54,11 @@ def asr_models_loop_full():
                         # import ipdb;ipdb.set_trace()
                         # lat_sig[i % n, i // n, k] = [float(_a[3][:-1]), float(_a[6]), float(_a[9][:-1]), float(_a[11]), i // n, float(_a[0].split('_')[-1].rstrip(':'))]
                         # lat_sig[i % n, i // n, k] = [float(_a[10][:-1]), 0, float(_a[16][:-1]), float(_a[-1]), i // n, float(_a[7].split('_')[-1].rstrip(':'))]
-                        lat_sig[i % n, i // n, k] = [float(_a[3][:-1]), 0, float(_a[9][:-1]), float(_a[-1]), i // n, float(_a[0].split('_')[-1].rstrip(':'))]
+                        try:
+                            lat_sig[i % n, i // n, k] = [float(_a[3][:-1]), 0, float(_a[6][:-1]), float(_a[-1]), i // n, float(_a[0].split('_')[-1].rstrip(':'))]
+                        except:
+                            # lat_sig[i % n, i // n, k] = [float(_a[3][:-1]), 0, float(_a[9][:-1]), float(_a[-1]), i // n, float(_a[0].split('_')[-1].rstrip(':'))]
+                            lat_sig[i % n, i // n, k] = [float(_a[10][:-1]), 0, float(_a[16][:-1]), float(_a[-1]), i // n, float(_a[7].split('_')[-1].rstrip(':'))]
                         ia += 1
                     break
 
@@ -136,7 +143,11 @@ def asr_models_loop_full():
     _lats = np.array([lat_sig[0, j, :] for j in range(lat_sig.shape[1]) if (lat_sig[0, j, 0] != 0 and lat_sig[0, j, 3] > thres)])
     stds.append(np.std(_lats[:, 0]))
 
-    # import ipdb;ipdb.set_trace()
+    _lats_dec = np.array([_lats[j, :] for j in range(_lats.shape[0]) if _lats[j, -2] > 31])
+    print(_lats_dec.shape[0])
+    print(np.mean(_lats_dec[:, 3]))
+
+    import ipdb;ipdb.set_trace()
 
     scatter = ax.scatter(_lats[:, 0], _lats[:, 4], c= _lats[:, 4], cmap='brg', marker='.', s=15, norm=norm)
     cbar = plt.colorbar(scatter, ax=ax, label='layers')
@@ -144,15 +155,15 @@ def asr_models_loop_full():
     #for j in range(_lats.shape[0]):
     #    ax.annotate(j+1, (_lats[j, 0], _lats[j, 3]))
 
-    plt.ylabel('Layer number')
-    plt.xlabel('Latencies (ms)')
-    plt.title(f'Threshold -log(p-value): {thres}')
-    plt.xlim(-200, 800)
-    plt.ylim(-1, 65)
-    # plt.legend()
-    # plt.xlim(-10, 60)
-    # plt.savefig(f'kymata-toolbox-data/output/scatter_plot/whisper_all_{size}_colour_layer.png', dpi=600)
-    plt.savefig(f'/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/size/large-v2/fc2/en_de.png', dpi=600)
+    # plt.ylabel('Layer number')
+    # plt.xlabel('Latencies (ms)')
+    # plt.title(f'Threshold -log(p-value): {thres}')
+    # plt.xlim(-200, 800)
+    # plt.ylim(-1, 65)
+    # # plt.legend()
+    # # plt.xlim(-10, 60)
+    # # plt.savefig(f'kymata-toolbox-data/output/scatter_plot/whisper_all_{size}_colour_layer.png', dpi=600)
+    # plt.savefig(f'/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/ccn_paper/ru_en.png', dpi=600)
 
 if __name__ == '__main__':
     asr_models_loop_full()
--- submit_gridsearch_large_ru_ind_2.sh
+++ submit_gridsearch_large_ru_ind_2.sh
@@ -2,13 +2,13 @@
 
 ###
 # To run gridsearch on the queue at the CBU, run the following command in command line:
-#   sbatch submit_gridsearch_large_ru_en.sh
+#   sbatch submit_gridsearch_large_ru_ind_2.sh
 ###
 
 
 #SBATCH --job-name=gridsearch
-#SBATCH --output=kymata-core-data/output/ccn_paper/ru_en/pilot_04/log/slurm_log_%a.txt
-#SBATCH --error=kymata-core-data/output/ccn_paper/ru_en/pilot_04/log/slurm_log_%a.txt
+#SBATCH --output=kymata-core-data/output/ccn_paper/ru/meg15_0070/log/slurm_log_%a.txt
+#SBATCH --error=kymata-core-data/output/ccn_paper/ru/meg15_0070/log/slurm_log_%a.txt
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=200G
@@ -36,7 +36,7 @@ apptainer exec \
       export VENV_PATH=~/poetry/ ; \
       export VIRTUAL_ENV=/imaging/woolgar/projects/Tianyi/virtualenvs/kymata-toolbox-jvBImMG9-py3.11/ ; \
       \$VENV_PATH/bin/poetry run python -m kymata.invokers.run_gridsearch \
-        --config dataset4.1.yaml \
+        --config dataset3.yaml \
         --input-stream auditory \
         --plot-top-channels \
         --transform-path '/imaging/projects/cbu/kymata/data/dataset_4.1-russian_narrative_english_native/predicted_function_contours/asr_models/whisper_fc2_and_final_layer_norm/whisper_large_teacher' \
@@ -45,7 +45,7 @@ apptainer exec \
         --n-derangements 5 \
         --asr-option 'all' \
         --mfa True \
-        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/ru_en/pilot_04/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
-        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/ru_en/pilot_04/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
-        --single-participant-override pilot_04 \
+        --save-plot-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/ru/meg15_0070/plot/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --save-expression-set-location '/imaging/projects/cbu/kymata/analyses/tianyi/kymata-core/kymata-core-data/output/paper/ru/meg15_0070/expression_set/${layer_num[$(($SLURM_ARRAY_TASK_ID))]}' \
+        --single-participant-override meg15_0070 \
   "
