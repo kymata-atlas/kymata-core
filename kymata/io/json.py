375f35cac8b321c8fb5eca600ecb562a8d3f408e
Cai Wingfield <cai@caiwingfield.net>
2025-06-20 11:28:57 +0100

    Clear up gpt verbosity

--- kymata/io/json.py
+++ kymata/io/json.py
@@ -4,7 +4,7 @@ from typing import Any
 import numpy as np
 from networkx import Graph
 
-from kymata.entities.expression import ExpressionPoint
+from kymata.ippm.graph import IPPMNode
 
 
 class NumpyJSONEncoder(JSONEncoder):
@@ -27,27 +27,25 @@ def serialise_graph(graph: Graph) -> dict:
     from the IPPMNode objects that constitute the graph.
     """
     nodes = []
-    for node_obj in graph.nodes:
-        # node_obj is now an IPPMNode instance
+    node: IPPMNode
+    for node in graph.nodes:
         nodes.append({
-            "node_id": node_obj.node_id,
-            "is_input_node": node_obj.is_input_node,
-            "hemisphere": node_obj.hemisphere,
-            "channel": node_obj.channel,
-            "latency": node_obj.latency,
-            "transform": node_obj.transform,
-            "logp_value": node_obj.logp_value,
+            "node_id": node.node_id,
+            "is_input_node": node.is_input_node,
+            "hemisphere": node.hemisphere,
+            "channel": node.channel,
+            "latency": node.latency,
+            "transform": node.transform,
+            "logp_value": node.logp_value,
         })
 
     edges = []
-    # The source and target of an edge are the IPPMNode objects themselves
-    for source_obj, target_obj in graph.edges:
+    for source, target in graph.edges:
         edges.append({
-            "source": source_obj.node_id,
-            "target": target_obj.node_id,
+            "source": source.node_id,
+            "target": target.node_id,
         })
 
-    # Assemble the final dictionary in the same format as before
     return {
         "directed": True,
         "multigraph": False,
