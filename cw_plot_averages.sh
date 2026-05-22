c73d7153348325f38ea1dfb0494b0fea43e12f34
Cai Wingfield <cai@caiwingfield.net>
2026-05-22 11:35:56 +0100

    Update wrapper scripts

--- cw_plot_averages.sh
+++ cw_plot_averages.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-echo English
+echo English Encoder
 
 poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/ecog_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/"
-poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/eeg_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/"
-poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/meg_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/"
-poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/ecog_sig_neurons_layer29_neuron3584.npy" \
-     "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/eeg_sig_neurons_layer29_neuron3584.npy" \
-     "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/meg_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen/"
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_english/sensor/encoder/meg_sig_neurons_layer32_neuron1280.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/english_encoder/scatter_split/"
 
-echo Russian
+echo English Decoder
 
 poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/eeg_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/"
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_english/sensor/decoder_text/meg_sig_neurons_layer29_neuron3584.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/english_decoder/scatter_split/"
+
+echo Russian Encoder
+
 poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/meg_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/"
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_russian/sensor/encoder/meg_sig_neurons_layer32_neuron1280.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/russian_encoder/scatter_split/"
 poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
-  -i "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/eeg_sig_neurons_layer29_neuron3584.npy" \
-     "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/meg_sig_neurons_layer29_neuron3584.npy" \
-  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/scatter_split_qwen_russian/"
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_russian/sensor/encoder/eeg_sig_neurons_layer32_neuron1280.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/russian_encoder/scatter_split/"
+poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_russian/sensor/encoder/meg_sig_neurons_layer32_neuron1280.npy" \
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_russian/sensor/encoder/eeg_sig_neurons_layer32_neuron1280.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/russian_encoder/scatter_split/"
+
+echo Russian Decoder
+
+poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_russian/sensor/decoder_text/meg_sig_neurons_layer29_neuron3584.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/russian_decoder/scatter_split/"
+
+echo English/Russian Encoder
+
+poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
+   -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_english_russian/sensor/all/encoder/meg_sig_neurons_layer32_neuron1280.npy" \
+   -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/cross_encoder/scatter_split/"
+
+echo English/Russian Decoder
+
+poetry run python kymata/invokers/language_neuron_fit.py -m 4 \
+  -i "/imaging/projects/cbu/kymata/analyses/tianyi/russian-english/kymata-core/kymata-core-data/output/qwen_english_russian/sensor/all/decoder_text/meg_sig_neurons_layer29_neuron3584.npy" \
+  -o "/imaging/projects/cbu/kymata/analyses/cai/kymata-core/kymata-core-data/tianyi/sensor/cross_decoder/scatter_split/"
--- cw_scatter.sh
+++ cw_scatter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-in="/imaging/projects/cbu/kymata/analyses/cai/kymata-core-tianyi-temp/kymata-core-data/tianyi/sensor/logs_split/"
-out="/imaging/projects/cbu/kymata/analyses/cai/kymata-core-tianyi-temp/kymata-core-data/tianyi/sensor/scatter_split/"
+in="/Users/cai/Dox/Work/Kymata lab/Code/kymata-core/kymata-core-data/tianyi/sensor/encoder/log_split"
+out="/Users/cai/Dox/Work/Kymata lab/Code/kymata-core/kymata-core-data/tianyi/sensor/encoder/scatter_split/"
 
 declare -a modalities=("eeg" "meg")
 declare -a axes=("latency" "neuron")
 
 for modality in "${modalities[@]}"; do
   for axis in "${axes[@]}"; do
-    poetry run python -m kymata.invokers.neuron_scatter \
+    poetry run python -m kymata.invokers.language_neuron_scatter \
       --dataset "${modality}" --x-axis "${axis}"\
       -i "${in}" -o "${out}"
   done
