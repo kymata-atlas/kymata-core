14bb7f682af8f0ba4757742a91e05f451d4135f2
Cai Wingfield <cai@caiwingfield.net>
2026-07-17 11:19:08 +0100

    Function to create an anchored gradient colormap
    (cherry picked from commit 7c58b483aac588a72b0019ec1624825744bf2796)

--- kymata/plot/color.py
+++ kymata/plot/color.py
@@ -1,4 +1,4 @@
-from matplotlib.colors import to_rgb, to_hex
+from matplotlib.colors import to_rgb, to_hex, LinearSegmentedColormap
 from numpy import linspace
 
 transparent = (0, 0, 0, 0)
@@ -63,3 +63,28 @@ def constant_color_dict(transforms: list[str], color) -> dict[str, str]:
     The returned dictionary is compatible with color specifications for expression plots.
     """
     return gradient_color_dict(transforms, color, color)
+
+
+def anchored_gradient_colormap(name: str, anchor_values: list[float | int], colors: list):
+    """
+    Creates a colormap which is a stack of unequal-height gradients with segment endpoints at specific values.
+
+    Args:
+        name: The name of the colormap
+        anchor_values: List of anchor values for gradient segment boundaries (including first and last endpoints)
+        colors: Colours for each boundary point
+
+    Raises:
+        ValueError if `layer_anchors` and `colors` aren't of the same length.
+
+    Returns:
+        A LinearSegmentedColormap
+    """
+    if len(anchor_values) != len(colors):
+        raise ValueError("Specify one color exactly for each layer anchor")
+
+    # Convert layer anchors to value ranges
+    vmin, vmax = anchor_values[0], anchor_values[-1]
+    positions = [(v - vmin) / (vmax - vmin) for v in anchor_values]
+
+    return LinearSegmentedColormap.from_list(name, list(zip(positions, colors)))
